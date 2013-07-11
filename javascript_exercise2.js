b380f81a3b4969e0251292e0b13a985ffed5b777
sanjeev <sanjeev@vinsol.com>
2013-07-11 13:05:47 +0530

    Added new exercise3 and issues fixed in exercise2

--- javascript_exercise2.js
+++ javascript_exercise2.js
@@ -1,33 +1,34 @@
 
-function check(obj) {
-  "use strict";
-  document.getElementById("none").checked = false;
-  var x = document.getElementsByClassName("checkbox"), count = checkForMax3(x);
-  obj.checked = false;
-  if (count > 3) {
-    alert("Only 3 days can be selected.");
-  }
-  else {
-    obj.checked = true;
-  }
-}
-
-function checkForMax3(x) {
-  "use strict";
-  var c = 0, j = x.length - 1;
-  while (j >= 0) {
-    if (x[j].checked === true) {
-      c += 1;
+(function selectMax3Days(doc) {
+  check = function (obj) {
+    "use strict";
+    var days = doc.getElementsByClassName("days"), checked = obj.checked;
+    doc.getElementById("none").checked = false;
+    obj.checked = false;
+    if (!moreThan3Selected(days)) {
+      obj.checked = checked;
     }
-    j -= 1;
   }
-  return c;
-}
 
-function uncheckAll() {
-  "use strict";
-  var x = document.getElementsByClassName("checkbox"), j = x.length;
-  for (var i=0;i<j;i++) {
-    x[i].checked = false;
+   moreThan3Selected = function (days) {
+    "use strict";
+    var c = 0, j = days.length;
+    while (--j >= 0 ) {
+      if (days[j].checked === true) {
+        if (++c >= 3) {
+          alert("Only 3 days can be selected.");
+          return true;
+        }
+      }
+    }
+    return false;
   }
-}
+
+  uncheckAll = function () {
+    "use strict";
+    var days = doc.getElementsByClassName("days"), j = days.length;
+    while (--j >= 0) {
+      days[j].checked = false;
+    }
+  }
+})(document);
