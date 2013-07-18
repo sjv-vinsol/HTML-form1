3bf4a84ee4944a451f8980507edc0e3858b272fd
sanjeev <sanjeev@vinsol.com>
2013-07-19 01:55:55 +0530

    use object oriented approach and removed unnecessary form validation conditions

--- javascript_exercise9.js
+++ javascript_exercise9.js
@@ -1,42 +1,30 @@
 window.addEventListener("load", function() {
   var main = {
     form: {
-      loginId: document.getElementById("loginId"),
-      name: document.getElementById("name"),
       email: document.getElementById("email"),
-      timezone: document.getElementById("timezone"),
       homepage: document.getElementById("homepage"),
-      about: document.getElementById("about"),
-      receiveNotification: document.getElementById("notification"),
       register: document.getElementById("register")
     },
 
-    label: document.getElementsByTagName("label"),
-
     validateForm: function (e) {
-      var msg="", elem = form1.elements;
-      for (i=0; i<elem.length-2; i++) {
-        if (elem[i].value == "") {
-          alert(main.label[i].innerHTML+ " can't be empty");
-          e.preventDefault();
-        }
-      }
-      if (main.form.about.value.length < 50 ) {
-        alert("Minimum 50 characters are required in About Us field");
+      "use strict";      
+      var elem = form1.elements;
+      var isValidEmail = main.isInvalid("email", main.form.email.value);
+      var isValidUrl = main.isInvalid("url", main.form.homepage.value);
+      if ( isValidEmail || isValidUrl) {
         e.preventDefault();
       }
-      var emailRegex = /^\w([.$&_]*\w+)*@([a-zA-Z]+\.)+[a-zA-Z]+$/;
-      var urlRegex = /^((https?|ftp):\/\/)([a-zA-Z]+\.)+[a-zA-Z]+\/[^\s]*$/;
-      if (main.form.email.value && !emailRegex.test(main.form.email.value)) {
-        alert("Invalid Email");
-        e.preventDefault;
-      }
-      if (main.form.homepage.value && !urlRegex.test(main.form.homepage.value)) {
-        alert("Invalid Url");
-        e.preventDefault;
-      }
     },
+
+    isInvalid: function(str, value) {
+      "use strict";
+      var regex = (str == "email") ? /^\w([.$&_]*\w+)*@([a-zA-Z]+\.)+[a-zA-Z]+$/ : /^((https?|ftp):\/\/)([a-zA-Z]+\.)+[a-zA-Z]+\/[^\s]*$/
+      if (!regex.test(value)) {
+        alert("Invalid "+str);
+        return true;
+      }
+    }
   }
-  // bind click event to submit button 
+  // bind click event to submit button
   main.form.register.addEventListener("click", main.validateForm);
 })
