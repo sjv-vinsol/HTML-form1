66e3aafdf0ae0a04a1d4e8bec65bd5f787870738
sanjeev <sanjeev@vinsol.com>
2013-07-19 15:31:36 +0530

    Implemented the changes specified
    1. Indent the code
    2. Added a alert box before displaying to html file
    3. Change the regex so as to escape all special char and digits.
    4. Function name "MatchAndCheck" is changed to extractNameMatchingRegex.
    5. Used DRY coading.

--- javascript_exercise6.js
+++ javascript_exercise6.js
@@ -1,17 +1,18 @@
 window.addEventListener("load", function() {
-  do {
-    var fname = prompt("First Name");
-   } while (!(fname = MatchAndCheck(fname)));
-  do {
-      var lname = prompt("Last Name");
-     } while (!(lname = MatchAndCheck(lname)));
-  document.write("Hello "+fname+" "+lname+".");
+  var namePrompt = ["First Name", "Last Name"], name= [], len = 0;
+  for(len=0;len<namePrompt.length;len++) {
+    do {
+      name[len] = prompt(namePrompt[len]);
+    } while (!(name[len] = extractNameMatchingRegex(name[len])));
+  }
+  alert("Hello "+name[0]+" "+name[1]+".");
+  document.write("Hello "+name[0]+" "+name[1]+".");
 
-  function MatchAndCheck(name) {
+  function extractNameMatchingRegex(name) {
     "use strict";
     if (name==null) {
       document.write("Please reload the page to start again");
     }
-    return name.match(/[a-zA-Z].*[a-zA-Z]/);
+    if (name.match(/[a-z]+/ig)) return name.match(/[a-z]+/ig).join(" ");
   }
 });
