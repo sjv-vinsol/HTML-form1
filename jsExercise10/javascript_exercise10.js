84d591c48afe2fa237a70630b9aa3ff6e8d0ed7b
sanjeev <sanjeev@vinsol.com>
2013-07-23 12:55:30 +0530

    Changes done as specified :
    1. Created object first and then call methods on it.
    2. Optimize the code.
    3. remove function submitForm() and added method isNumeric.

--- jsExercise10/javascript_exercise10.js
+++ jsExercise10/javascript_exercise10.js
@@ -1,28 +1,18 @@
 window.addEventListener("load", function() {
   var main = {
-    submitForm: function (e, formName, fieldObjArr) {
-      "use strict";
-      var form = new main.formObj(formName, fieldObjArr);
-      if (form.valid) {
-        form.submit();
-      } else e.preventDefault();
-    },
-
-    formObj: function (formName, fieldObjArr) {
+    formObj: function (formName) {
       "use strict";
       this.firstNameObj = document.forms[formName].fname;
       this.resultObj = document.forms[formName].result;
-      var regex = /(^([+]|[-])?\d+(\.[\d]+)?$|^[\da-f]+$)/;
-      if (regex.test(this.firstNameObj.value)) {
-        this.resultObj.value = "true";
-        this.valid = true;
-      }else {
-        this.resultObj.value = "false";
-        this.valid = false;
+      this.isNumeric = function() {
+        var regex = /^([+]|[-])?\d+(\.[\d]+)?$/;
+        return regex.test(this.firstNameObj.value);
       }
     }
   }
   document.form1.addEventListener("submit", function(e) {
-    main.submitForm(e, "form1", ["fname", "result"]);
+    var form = new main.formObj("form1");
+    (form.isNumeric()) ? (form.resultObj.value = "true") : (form.resultObj.value = "false");
+    if (!form.isNumeric()) e.preventDefault();
   })
 })
