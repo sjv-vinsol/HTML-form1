8b2ca6f780e38b5ca092f71b15369430af3fa3a1
sanjeev <sanjeev@vinsol.com>
2013-07-25 00:58:06 +0530

    optimized the code, Extract domain and subdomain from regex

--- js esercise11/javascript_exercise11.js
+++ js esercise11/javascript_exercise11.js
@@ -1,32 +1,28 @@
 window.addEventListener("load", function() {
-  formObj = function(formName) {
-    "use stict";
-    this.url = document.forms[formName].url;
-    this.extractDomainAndSubdomain = function (){
-      // regexStr holds the regex to match string containing only domain and subdomain. Ex: http://help.google.com/, regexStr will match "help.google.com".
-      var regexStr = /[a-z]+(\.[a-z]+)+/, regexDomain = /[a-z]+(\.[a-z]+)$/;
-      var str = this.url.value.match(regexStr)[0];
-      var domain = str.match(regexDomain)[0];
-      this.msg = "Domain :" + domain;
-      var subDomain = str.substring(0, (str.length - domain.length)-1);
-      if (subDomain) {
-        this.msg += "\nSubdomain : " + subDomain;
+  var main = {
+    regex: /^((https?|ftp):\/\/)?(([\w]+\.)*)([\w]+\.[\w]+)([\/][^\s]*)*$/,
+    formObj: function(formName) {
+      "use stict";
+      this.url = document.forms[formName].url;
+      this.hasValidUrl = function () {
+        if (this.url.value.match(main.regex)) return true
+          else return false;
+      }
+      this.extractDomainAndSubdomain = function () {
+        var str = this.url.value.match(main.regex);
+        return ([str[5],str[3].slice(0,-1)]);
       }
     }
-    this.isValidUrl = function () {
-      var regex = /^((https?|ftp):\/\/)([a-zA-Z]+\.)+[a-zA-Z]+\/[^\s]*$/;
-      return regex.test(this.url.value);
-    }
   }
-  // Binding a function on click event of submit button
+
   document.getElementById("submit").addEventListener("click", function(e) {
-    var form = new formObj("form1");
-    if (form.isValidUrl()) {
-      form.extractDomainAndSubdomain();
-      alert(form.msg);
-    } else {
+    var form = new main.formObj("form1");
+    if (!form.hasValidUrl()) {
       alert("Please enter a valid Url");
       e.preventDefault();
+    } else {
+      domainSubdomainArr = form.extractDomainAndSubdomain();
+      (domainSubdomainArr[1]) ? alert("Domain: " + domainSubdomainArr[0] + "\nSub Domain: " + domainSubdomainArr[1]) : alert("Domain: " + domainSubdomainArr[0]);
     }
   });
 })
