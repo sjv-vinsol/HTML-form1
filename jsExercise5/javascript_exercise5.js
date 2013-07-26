215bc02e5ca16be5a076b767a3f0f814349a04ba
sanjeev <sanjeev@vinsol.com>
2013-07-26 13:51:31 +0530

    Changes done
    1.Devide form fields into two hash. One is for empty validation and other
    hash contains input fields other than first hash.
    2. Removed length-2 from loop.

--- jsExercise5/javascript_exercise5.js
+++ jsExercise5/javascript_exercise5.js
@@ -1,20 +1,23 @@
 window.addEventListener("load", function() {
   var main = {
-    form: {
+    fieldsForEmptyValidation: {
       loginId: document.getElementById("loginId"),
       name: document.getElementById("name"),
       email: document.getElementById("email"),
       timezone: document.getElementById("timezone"),
       homepage: document.getElementById("homepage"),
       about: document.getElementById("about"),
+    },
+
+    otherInputFields: {
       receiveNotification: document.getElementById("notification"),
       register: document.getElementById("register")
     },
 
     validateForm: function (e) {
-      var elem = Object.keys(main.form), hasError = false, status = null;
-      for (i=0; i<elem.length-2; i++) {
-        status = main.isEmpty(main.form[elem[i]]);
+      var elem = Object.keys(main.fieldsForEmptyValidation), hasError = false, status = null;
+      for (i=0; i<elem.length; i++) {
+        status = main.isEmpty(main.fieldsForEmptyValidation[elem[i]]);
         hasError = (status) ? status : hasError;
       }
 
@@ -32,16 +35,16 @@ window.addEventListener("load", function() {
     },
 
     characterCountCheck: function () {
-      if (main.form.about.value.length < 50 ) {
+      if (main.fieldsForEmptyValidation.about.value.length < 50 ) {
         alert("Minimum 50 characters are required in About Us field");
       }
     },
 
     confirmReceiveNotification: function () {
-      var confirmText = (main.form.receiveNotification.checked) ? "CONFIRM: You are subscribed to receive notification":"CONFIRM:You are not subscribed to receive notification";
+      var confirmText = (main.otherInputFields.receiveNotification.checked) ? "CONFIRM: You are subscribed to receive notification":"CONFIRM:You are not subscribed to receive notification";
       return confirm(confirmText);
     }
   }
   // bind click event to submit button 
-  main.form.register.addEventListener("click", main.validateForm);
+  main.otherInputFields.register.addEventListener("click", main.validateForm);
 })
