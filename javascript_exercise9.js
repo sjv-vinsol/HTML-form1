2884f3c3bad5a2a9c0a5d96b71ee2dfcb9868eec
sanjeev <sanjeev@vinsol.com>
2013-07-30 15:34:55 +0530

    optimized url regex and code

--- javascript_exercise9.js
+++ javascript_exercise9.js
@@ -3,42 +3,32 @@ window.addEventListener("load", function() {
     submitForm: function (e , formName, validateFieldArr) {
       "use strict";
       var form = new main.formObj(formName, validateFieldArr);
-      if (form.valid) {
-        form.submit();
-      } else e.preventDefault();
+      if (form.isValid()) form.submit();
+      else e.preventDefault();
     },
 
     formObj: function (formName, validateFieldArr) {
       "use strict";
       var validFieldCount = 0;
-      var email = document.forms[formName].email;
-      var homepage = document.forms[formName].url;
-
-      var validateUsingRegex = function (currfieldObj, regex) {
-        if (regex.test(currfieldObj.value)) {
-          validFieldCount++;
-          currfieldObj.className = "normal";
+      this.email = document.forms[formName].email;
+      this.homepage = document.forms[formName].url;
+      this.validateUsingRegex = function (currfieldObj, regex) {
+        if (regex.test(currfieldObj.value) && ++validFieldCount) {
+          currfieldObj.classList.add("normal");
         } else {
           alert(currfieldObj.name + " is invalid");
-          main.paintRed(currfieldObj);
+          currfieldObj.classList.add("error");
         }
       }
-
-      validateUsingRegex(email, /^\w([\.\$\?\&\_]?\w+)*@([a-zA-Z]+\.)+[a-zA-Z]+$/);
-      validateUsingRegex(homepage, /^((https?|ftp):\/\/)([a-zA-Z]+\.)+[a-zA-Z]+\/[^\s]*$/);
-
-      if (validFieldCount == validateFieldArr.length) {
-        this.valid = true;
-      } else this.valid = false;
+      this.isValid = function() {
+        this.validateUsingRegex(this.email, /^\w([\.\$\?\&\_]?\w+)*@([a-zA-Z]+\.)+[a-zA-Z]+$/);
+        this.validateUsingRegex(this.homepage, /^[\s]*(?:(?:https?|ftp):\/\/)?(?:[\w]+\.)*[\w]+\.[\w]+(?:[\/]+[^\s]*)*[\s]*$/);
+        if (validFieldCount == validateFieldArr.length) return true;
+        else return false;
+      }
     },
-
-    paintRed: function (currfieldObj) {
-      "use strict";
-      currfieldObj.className = "error";
-    }
   }
   document.getElementById("register").addEventListener("click", function(e) {
-    var fieldArr = ["email", "url"];
-    main.submitForm(e, "form1", fieldArr);
+    main.submitForm(e, "form1", ["email", "url"]);
   });
 });
