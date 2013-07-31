a630f2776a9859b2e3ff3e91b0dab48f6a86d3ea
sanjeev <sanjeev@vinsol.com>
2013-07-31 20:09:59 +0530

    Interchange names of two js files

--- javascript_exercise7_2.js
+++ javascript_exercise7_2.js
@@ -2,7 +2,12 @@ var main = {
   checkAndOpenUrl: function (url) {
     "use strict";
     if( url) {
-      return (window.open(url,"popup","height=450,width=400,top=100,left=400,status=no,menubar=no"));
+      var obj = window.open(null,"popup","height=450,width=400,top=100,left=400,status=no,menubar=no");
+      var iframe = obj.document.getElementsByTagName("body")[0].appendChild(obj.document.createElement("iframe"));
+      iframe.src = url; iframe.scrolling = "no";
+      iframe.setAttribute("style", "height:100%;width:100%;border:0px");
+      obj.document.getElementsByTagName("body")[0].style.overflow = "hidden";
+      return true;      
     } else if (url == null) return true;
     else if (url == "") return alert("url is empty. Try again!!");
   }
