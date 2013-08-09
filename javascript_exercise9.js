31e54f5e6cb8a4ca577da0f6936c44df740dc53a
sanjeev <sanjeev@vinsol.com>
2013-08-09 19:25:42 +0530

    Implemented specified changes
    1. Change the name of formObj to form
    2. Used variable invalidForm which stores boolean value.
    3. Removed method submitForm.
    4. Used classList method to remove class "Normal".

--- javascript_exercise9.js
+++ javascript_exercise9.js
@@ -1,34 +1,31 @@
 window.addEventListener("load", function() {
   var main = {
-    submitForm: function (e , formName, validateFieldArr) {
+    form: function (formName, validateFieldArr) {
       "use strict";
-      var form = new main.formObj(formName, validateFieldArr);
-      if (form.isValid()) form.submit();
-      else e.preventDefault();
-    },
-
-    formObj: function (formName, validateFieldArr) {
-      "use strict";
-      var validFieldCount = 0;
+      var invalidForm = true;
       this.email = document.forms[formName].email;
       this.homepage = document.forms[formName].url;
+      this.emailRegex = /^\w([\.\$\?\&\_]?\w+)*@([a-zA-Z]+\.)+[a-zA-Z]+$/;
+      this.urlRegex = /^[\s]*(?:(?:https?|ftp):\/\/)?(?:[\w]+\.)*[\w]+\.[\w]+(?:[\/]+[^\s]*)*[\s]*$/;
       this.validateUsingRegex = function (currfieldObj, regex) {
-        if (regex.test(currfieldObj.value) && ++validFieldCount) {
+        if (regex.test(currfieldObj.value)) {
           currfieldObj.classList.add("normal");
         } else {
+          invalidForm = false;
           alert(currfieldObj.name + " is invalid");
-          currfieldObj.classList.add("error");
+          currfieldObj.classList.add("error"); currfieldObj.classList.remove("normal");
         }
       }
       this.isValid = function() {
-        this.validateUsingRegex(this.email, /^\w([\.\$\?\&\_]?\w+)*@([a-zA-Z]+\.)+[a-zA-Z]+$/);
-        this.validateUsingRegex(this.homepage, /^[\s]*(?:(?:https?|ftp):\/\/)?(?:[\w]+\.)*[\w]+\.[\w]+(?:[\/]+[^\s]*)*[\s]*$/);
-        if (validFieldCount == validateFieldArr.length) return true;
-        else return false;
+        this.validateUsingRegex(this.email, this.emailRegex);
+        this.validateUsingRegex(this.homepage, this.urlRegex);
+        return invalidForm;
       }
     },
   }
   document.getElementById("register").addEventListener("click", function(e) {
-    main.submitForm(e, "form1", ["email", "url"]);
+    var form = new main.form("form1", ["email", "url"]);
+    if (form.isValid()) form.submit();
+    else e.preventDefault();
   });
 });
