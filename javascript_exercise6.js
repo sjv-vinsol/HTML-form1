065765aec5bab35f884d0b10628bf9a40b174f60
sanjeev <sanjeev@vinsol.com>
2013-08-09 18:38:48 +0530

    Optimisez code

--- javascript_exercise6.js
+++ javascript_exercise6.js
@@ -1,18 +1,19 @@
 window.addEventListener("load", function() {
-  var namePrompt = ["First Name", "Last Name"], name= [], len = 0;
+  var namePrompt = ["First Name", "Last Name"], name= [], len = 0, msg = "";
   for(len=0;len<namePrompt.length;len++) {
     do {
       name[len] = prompt(namePrompt[len]);
     } while (!(name[len] = extractNameMatchingRegex(name[len])));
   }
-  alert("Hello "+name[0]+" "+name[1]+".");
-  document.write("Hello "+name[0]+" "+name[1]+".");  
+  msg = "Hello "+name[0]+" "+name[1]+".";
+  alert(msg); document.write(msg);
 });
 
 function extractNameMatchingRegex(name) {
   "use strict";
+  var matchedName = "";
   if (name==null) {
     document.write("Please reload the page to start again");
   }
-  if (name.match(/[a-z]+/ig)) return name.match(/[a-z]+/ig).join(" ");
+  if (matchedName = name.match(/[a-z]+/ig)) return matchedName.join(" ");
 }
