e6b87283d90be14f922851fe5674f7f694c653b7
sanjeev <sanjeev@vinsol.com>
2013-08-11 23:17:52 +0530

    Changes implemented
    1. Change class name from "form" to "Form"
    2. Optimized validateUsingRegex method.

--- javascript_exercise9.js
+++ javascript_exercise9.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", function() {
   var main = {
-    form: function (formName, validateFieldArr) {
+    Form: function (formName, validateFieldArr) {
       "use strict";
       var invalidForm = true;
       this.email = document.forms[formName].email;
@@ -8,23 +8,17 @@ window.addEventListener("load", function() {
       this.emailRegex = /^\w([\.\$\?\&\_]?\w+)*@([a-zA-Z]+\.)+[a-zA-Z]+$/;
       this.urlRegex = /^[\s]*(?:(?:https?|ftp):\/\/)?(?:[\w]+\.)*[\w]+\.[\w]+(?:[\/]+[^\s]*)*[\s]*$/;
       this.validateUsingRegex = function (currfieldObj, regex) {
-        if (regex.test(currfieldObj.value)) {
-          currfieldObj.classList.add("normal");
-        } else {
-          invalidForm = false;
-          alert(currfieldObj.name + " is invalid");
-          currfieldObj.classList.add("error"); currfieldObj.classList.remove("normal");
-        }
+        var isValidString = regex.test(currfieldObj.value);
+        if (!isValidString) alert(currfieldObj.name + " is invalid");
+        return isValidString;
       }
       this.isValid = function() {
-        this.validateUsingRegex(this.email, this.emailRegex);
-        this.validateUsingRegex(this.homepage, this.urlRegex);
-        return invalidForm;
+        return (this.validateUsingRegex(this.email, this.emailRegex) && this.validateUsingRegex(this.homepage, this.urlRegex));
       }
     },
   }
   document.getElementById("register").addEventListener("click", function(e) {
-    var form = new main.form("form1", ["email", "url"]);
+    var form = new main.Form("form1", ["email", "url"]);
     if (form.isValid()) form.submit();
     else e.preventDefault();
   });
