e3ea027af39b9f864c8aa7b81f151ab1e2d21941
sanjeev <sanjeev@vinsol.com>
2013-08-13 11:32:13 +0530

    Implemented following changes :
    1. Make regex variable global.
    2. Used operator '&' instead of "&&".

--- javascript_exercise9.js
+++ javascript_exercise9.js
@@ -1,3 +1,5 @@
+emailRegex = /^\w([\.\$\?\&\_]?\w+)*@([a-zA-Z]+\.)+[a-zA-Z]+$/;
+urlRegex = /^[\s]*(?:(?:https?|ftp):\/\/)?(?:[\w]+\.)*[\w]+\.[\w]+(?:[\/]+[^\s]*)*[\s]*$/;
 window.addEventListener("load", function() {
   var main = {
     Form: function (formName, validateFieldArr) {
@@ -5,16 +7,13 @@ window.addEventListener("load", function() {
       var invalidForm = true;
       this.email = document.forms[formName].email;
       this.homepage = document.forms[formName].url;
-      this.emailRegex = /^\w([\.\$\?\&\_]?\w+)*@([a-zA-Z]+\.)+[a-zA-Z]+$/;
-      this.urlRegex = /^[\s]*(?:(?:https?|ftp):\/\/)?(?:[\w]+\.)*[\w]+\.[\w]+(?:[\/]+[^\s]*)*[\s]*$/;
       this.validateUsingRegex = function (currfieldObj, regex) {
         var isValidString = regex.test(currfieldObj.value);
         if (!isValidString) alert(currfieldObj.name + " is invalid");
         return isValidString;
       }
       this.isValid = function() {
-        var temp = this.validateUsingRegex(this.email, this.emailRegex) 
-        return (this.validateUsingRegex(this.homepage, this.urlRegex) && temp);
+        return (this.validateUsingRegex(this.homepage, urlRegex) & this.validateUsingRegex(this.email, emailRegex));
       }
     },
   }
