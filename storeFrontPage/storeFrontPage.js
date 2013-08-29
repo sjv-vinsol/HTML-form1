76a3a6b01b021b1721ee4b33d88446357f5aab01
sanjeev <sanjeev@vinsol.com>
2013-08-29 17:38:28 +0530

    Removed eval function

--- storeFrontPage/storeFrontPage.js
+++ storeFrontPage/storeFrontPage.js
@@ -1,5 +1,5 @@
 var storeJSON = [{"name":"1","url":"1.jpg","color":"Yellow","brand":"BRAND A","sold_out":"1"},{"name":"2","url":"2.jpg","color":"Red","brand":"BRAND B","sold_out":"0"},{"name":"3","url":"3.jpg","color":"Green","brand":"BRAND D","sold_out":"0"},{"name":"4","url":"4.jpg","color":"Red","brand":"BRAND A","sold_out":"1"},{"name":"5","url":"5.jpg","color":"Blue","brand":"BRAND B","sold_out":"0"},{"name":"6","url":"6.jpg","color":"Green","brand":"BRAND C","sold_out":"0"},{"name":"7","url":"7.jpg","color":"Red","brand":"BRAND C","sold_out":"1"},{"name":"8","url":"8.jpg","color":"Blue","brand":"BRAND D","sold_out":"0"},{"name":"9","url":"9.jpg","color":"Yellow","brand":"BRAND A","sold_out":"0"},{"name":"10","url":"10.jpg","color":"Yellow","brand":"BRAND B","sold_out":"1"},{"name":"11","url":"11.jpg","color":"Green","brand":"BRAND D","sold_out":"0"},{"name":"12","url":"12.jpg","color":"Yellow","brand":"BRAND D","sold_out":"0"},{"name":"13","url":"13.jpg","color":"Blue","brand":"BRAND A","sold_out":"0"},{"name":"14","url":"14.jpg","color":"Blue","brand":"BRAND D","sold_out":"0"},{"name":"15","url":"15.jpg","color":"Green","brand":"BRAND B","sold_out":"0"},{"name":"16","url":"16.jpg","color":"Yellow","brand":"BRAND B","sold_out":"1"},{"name":"17","url":"17.jpg","color":"Green","brand":"BRAND A","sold_out":"1"},{"name":"18","url":"18.jpg","color":"Blue","brand":"BRAND D","sold_out":"1"},{"name":"19","url":"19.jpg","color":"Green","brand":"BRAND C","sold_out":"0"},{"name":"20","url":"20.jpg","color":"Yellow","brand":"BRAND A","sold_out":"0"}]
-var selectBoxIdCollection = ["color", "brand", "name", "stock"];
+var selectBoxIdCollection = ["color", "brand", "name", "sold_out"];
 var colorArr = [], brandArr = [], nameArr = [];
 //Populate color 
 function populateUniqueValueFor(conditionString) {
@@ -50,16 +50,16 @@ window.addEventListener("load", function() {
   (function displayProductsAndAddEvents() {
     displayNewProducts(storeJSON);
     document.getElementById("color").addEventListener("change", function() {
-      filterProductsBy("color", "storeJSON[i].color");
+      filterProductsBy("color");
     });
     document.getElementById("brand").addEventListener("change", function() {
-      filterProductsBy("brand", "storeJSON[i].brand");
+      filterProductsBy("brand");
     });
     document.getElementById("name").addEventListener("change", function() {
-      filterProductsBy("name", "storeJSON[i].name");
+      filterProductsBy("name");
     });
-    document.getElementById("stock").addEventListener("change", function() {
-      filterProductsBy("stock", "storeJSON[i].sold_out");
+    document.getElementById("sold_out").addEventListener("change", function() {
+      filterProductsBy("sold_out");
     });
   })();
 
@@ -89,12 +89,12 @@ window.addEventListener("load", function() {
     }
   }
 
-  function filterProductsBy(idOfSelectBox, conditionString) {
+  function filterProductsBy(idOfSelectBox) {
     var color = document.getElementById(idOfSelectBox).value;
     setDefaultSelectedIndex(idOfSelectBox);
     var productJSON = [];
     for(i=0;i<storeJSON.length;i++) {
-      if(color == eval(conditionString)) {
+      if(color == storeJSON[i][idOfSelectBox]) {
         productJSON.push(storeJSON[i]);
       }
     }
