e0d7a0b5c0bb0eef950c583e491885c367a26218
sanjeev <sanjeev@vinsol.com>
2013-10-03 16:07:41 +0530

    compleated exercise 1

---
+++ jQuery/Exercise-1/js/sandbox.js
@@ -0,0 +1,27 @@
+console.log("All div with class module  : ",$("div.module"));
+
+console.log("\n");
+
+console.log("Three selectors to select third item of #myList")
+console.log("First  :  ", $(":contains(List item 3)").last());
+console.log("Second  :  ",$("#myListItem"));
+console.log("Third  :  ", $("#myList li:eq(2)"));
+console.log("In first if text of third item is changed then this will no longer work.");
+console.log("Second one is the best one as it directly searches from Id");
+console.log("If Id is not present then third method is the best as it is independent of text OR Id OR class of item.");
+
+console.log("\n");
+
+console.log("Label for the search input using an attribute selector", $("label[for='q']"));
+
+console.log("\n");
+
+console.log("Hidden elements in the page", $("body").find(":hidden").not("script").length);
+
+console.log("\n");
+
+console.log("Image element that have alt attribute", $("img[alt]").length);
+
+console.log("\n");
+
+console.log("Odd trable rows in table body", $("tbody tr:even"));
