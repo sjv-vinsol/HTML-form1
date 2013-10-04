b7e59112c99b0c36316b3d3ac4e667924ac873d8
sanjeev <sanjeev@vinsol.com>
2013-10-04 15:55:09 +0530

    Removed unwanted console.log line breaks

--- jQuery/Exercise-1/js/sandbox.js
+++ jQuery/Exercise-1/js/sandbox.js
@@ -1,9 +1,7 @@
 $(document).ready(function () {
   console.log("All div with class module  : ", $("div.module"));
 
-  console.log("\n");
-
-  console.log("Three selectors to select third item of #myList")
+  console.log("\n\nThree selectors to select third item of #myList")
   console.log("First  :  ", $(":contains(List item 3)").last());
   console.log("Second  :  ", $("#myListItem"));
   console.log("Third  :  ", $("#myList li:eq(2)"));
@@ -11,19 +9,11 @@ $(document).ready(function () {
   console.log("Second one is the best one as it directly searches from Id");
   console.log("If Id is not present then third method is the best as it is independent of text OR Id OR class of item.");
 
-  console.log("\n");
+  console.log("\n\nLabel for the search input using an attribute selector", $("label[for='q']"));
 
-  console.log("Label for the search input using an attribute selector", $("label[for='q']"));
+  console.log("\n\nHidden elements in the page", $("body").find(":hidden").not("script").length);
 
-  console.log("\n");
-
-  console.log("Hidden elements in the page", $("body").find(":hidden").not("script").length);
-
-  console.log("\n");
-
-  console.log("Image element that have alt attribute", $("img[alt]").length);
-
-  console.log("\n");
+  console.log("\n\nImage element that have alt attribute", $("img[alt]").length);
 
   console.log("Odd trable rows in table body", $("tbody tr:even"));
 })
--- jQuery/Exercise-2/js/sandbox.js
+++ jQuery/Exercise-2/js/sandbox.js
@@ -4,24 +4,16 @@ $(document).ready( function () {
     console.log(val.alt);
   })
 
-  console.log("\n");
-
-  console.log("Select search field and add class to its parent");
+  console.log("\n\nSelect search field and add class to its parent");
   console.log( $("input[name='q']").parent().addClass("someClass"));
 
-  console.log("\n");
-
-  console.log("Remove class current and add it to next element");
+  console.log("\n\nRemove class current and add it to next element");
   console.log($("#myList .current").removeClass("current").next().addClass("current"));
 
-  console.log("\n");
-
-  console.log("select element inside #specials and traverse your way to the submit button");
+  console.log("\n\nSelect element inside #specials and traverse your way to the submit button");
   console.log($("#specials select").parent().next().children().first());
 
-  console.log("\n");
-
-  console.log("Add class 'current' to first line item of #slideshow and add class 'disabled' to its siblings");
+  console.log("\n\nAdd class 'current' to first line item of #slideshow and add class 'disabled' to its siblings");
   console.log($.each($("#slideshow > :first-child").addClass('current').siblings(), function (i, val){
     val.classList.add("disabled");
   }));
