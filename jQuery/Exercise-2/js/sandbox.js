ac3668b8c01d90dde8d7688c1a7dada3422cc05f
sanjeev <sanjeev@vinsol.com>
2013-10-04 12:57:34 +0530

    Added document.ready to exercise 1 and exercise 2

--- jQuery/Exercise-2/js/sandbox.js
+++ jQuery/Exercise-2/js/sandbox.js
@@ -1,26 +1,28 @@
-console.log("Log each image's alt attribute");
-$.each( $("img"), function(i, val) {
-  console.log(val.alt);
+$(document).ready( function () {
+  console.log("Log each image's alt attribute");
+  $.each( $("img"), function (i, val) {
+    console.log(val.alt);
+  })
+
+  console.log("\n");
+
+  console.log("Select search field and add class to its parent");
+  console.log( $("input[name='q']").parent().addClass("someClass"));
+
+  console.log("\n");
+
+  console.log("Remove class current and add it to next element");
+  console.log($("#myList .current").removeClass("current").next().addClass("current"));
+
+  console.log("\n");
+
+  console.log("select element inside #specials and traverse your way to the submit button");
+  console.log($("#specials select").parent().next().children().first());
+
+  console.log("\n");
+
+  console.log("Add class 'current' to first line item of #slideshow and add class 'disabled' to its siblings");
+  console.log($.each($("#slideshow > :first-child").addClass('current').siblings(), function (i, val){
+    val.classList.add("disabled");
+  }));
 })
-
-console.log("\n");
-
-console.log("Select search field and add class to its parent");
-console.log( $("input[name='q']").parent().addClass("someClass"));
-
-console.log("\n");
-
-console.log("Remove class current and add it to next element");
-console.log($("#myList .current").removeClass("current").next().addClass("current"));
-
-console.log("\n");
-
-console.log("select element inside #specials and traverse your way to the submit button");
-console.log($("#specials select").parent().next().children().first());
-
-console.log("\n");
-
-console.log("Add class 'current' to first line item of #slideshow and add class 'disabled' to its siblings");
-console.log($.each($("#slideshow > :first-child").addClass('current').siblings(), function(i, val){
-  val.classList.add("disabled");
-}));
