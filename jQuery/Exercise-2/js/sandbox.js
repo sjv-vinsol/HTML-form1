1618827502ea2a38f26ffce3971e953c4481ff71
sanjeev <sanjeev@vinsol.com>
2013-10-17 14:32:29 +0530

    Implemented below mentioned points :
    1. Optimized 4th and 5th point.
    2. Make the code DOM independent.

--- jQuery/Exercise-2/js/sandbox.js
+++ jQuery/Exercise-2/js/sandbox.js
@@ -1,20 +1,18 @@
 $(document).ready( function () {
-  console.log("Log each image's alt attribute");
+  console.log("1. Log each image's alt attribute");
   $.each( $("img"), function (i, val) {
     console.log(val.alt);
   })
 
-  console.log("\n\nSelect search field and add class to its parent");
-  console.log( $("input[name='q']").parent().addClass("someClass"));
+  console.log("\n\n2. Select search field and add class to its parent");
+  console.log( $("input[name='q']").closest("form").addClass("someClass"));
 
-  console.log("\n\nRemove class current and add it to next element");
-  console.log($("#myList .current").removeClass("current").next().addClass("current"));
+  console.log("\n\n3. Remove class current and add it to next element");
+  console.log($("#myList li.current").removeClass("current").nextAll("li").first().addClass("current"));
 
-  console.log("\n\nSelect element inside #specials and traverse your way to the submit button");
-  console.log($("#specials select").parent().next().children().first());
+  console.log("\n\n4. Select element inside #specials and traverse your way to the submit button");
+  console.log($("#specials").find("input[type='submit']"));
 
-  console.log("\n\nAdd class 'current' to first line item of #slideshow and add class 'disabled' to its siblings");
-  console.log($.each($("#slideshow > :first-child").addClass('current').siblings(), function (i, val){
-    val.classList.add("disabled");
-  }));
+  console.log("\n\n5. Add class 'current' to first line item of #slideshow and add class 'disabled' to its siblings");
+  console.log($("#slideshow > li:first").addClass('current').siblings().addClass('disabled'));
 })
