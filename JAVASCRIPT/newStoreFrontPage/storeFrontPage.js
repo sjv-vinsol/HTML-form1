eaade288ff9a64d3fd42b36560a0115ad58b6b2d
sanjeev <sanjeev@vinsol.com>
2013-10-23 11:41:16 +0530

    Add event handler on change only once and use build in sort method in js

--- JAVASCRIPT/newStoreFrontPage/storeFrontPage.js
+++ JAVASCRIPT/newStoreFrontPage/storeFrontPage.js
@@ -1,53 +1,31 @@
 var storeJSON = [{"name": "1", "url": "1.jpg", "color": "Yellow", "brand": "BRAND A", "sold_out": "1"}, {"name": "2", "url": "2.jpg", "color": "Red", "brand": "BRAND B", "sold_out": "0"}, {"name": "3", "url": "3.jpg", "color": "Green", "brand": "BRAND D", "sold_out": "0"}, {"name": "4", "url": "4.jpg", "color": "Red", "brand": "BRAND A", "sold_out": "1"}, {"name": "5", "url": "5.jpg", "color": "Blue", "brand": "BRAND B", "sold_out": "0"}, {"name": "6", "url": "6.jpg", "color": "Green", "brand": "BRAND C", "sold_out": "0"}, {"name": "7", "url": "7.jpg", "color": "Red", "brand": "BRAND C", "sold_out": "1"}, {"name": "8", "url": "8.jpg", "color": "Blue", "brand": "BRAND D", "sold_out": "0"}, {"name": "9", "url": "9.jpg", "color": "Yellow", "brand": "BRAND A", "sold_out": "0"}, {"name": "10", "url": "10.jpg", "color": "Yellow", "brand": "BRAND B", "sold_out": "1"}, {"name": "11", "url": "11.jpg", "color": "Green", "brand": "BRAND D", "sold_out": "0"}, {"name": "12", "url": "12.jpg", "color": "Yellow", "brand": "BRAND D", "sold_out": "0"}, {"name": "13", "url": "13.jpg", "color": "Blue", "brand": "BRAND A", "sold_out": "0"}, {"name": "14", "url": "14.jpg", "color": "Blue", "brand": "BRAND D", "sold_out": "0"}, {"name": "15", "url": "15.jpg", "color": "Green", "brand": "BRAND B", "sold_out": "0"}, {"name": "16", "url": "16.jpg", "color": "Yellow", "brand": "BRAND B", "sold_out": "1"}, {"name": "17", "url": "17.jpg", "color": "Green", "brand": "BRAND A", "sold_out": "1"}, {"name": "18", "url": "18.jpg", "color": "Blue", "brand": "BRAND D", "sold_out": "1"}, {"name": "19", "url": "19.jpg", "color": "Green", "brand": "BRAND C", "sold_out": "0"}, {"name": "20", "url": "20.jpg", "color": "Yellow", "brand": "BRAND A", "sold_out": "0"}];
+// Put the values to sort in below array. Only below mentioned values will be show in the dropdown to sort.
 var valuesToSort = ["color", "brand", "name", "sold_out"];
 
 function Store() {
   "use strict";
-  var getUniqueValueFor = function (attribute) {
-    var uniqueValueArr = [], productCount = storeJSON.length, count = 0, isUnique = true, i = 0;
-    for (i = 0; i < productCount; i++) {
-      count = uniqueValueArr.length;
-      isUnique = true;
-      while (count-- && isUnique) {
-        if (storeJSON[i][attribute] == uniqueValueArr[count]) { isUnique = false; }
-      }
-      if (isUnique) { uniqueValueArr.push(storeJSON[i][attribute]); }
-    }
-    return uniqueValueArr.sort();
-  };
-
-  // Stores the attribure value as array in store.
-  this.populateValuesOfArrributes = function() {
-    var length = valuesToSort.length
-    while (length--) {
-      var attribute = valuesToSort[length]
-      if (attribute != "name") this[attribute] = getUniqueValueFor(attribute);
-      else this.name = getNames();
-    } 
-  }
-
-  var getNames = function () {
-    var length = storeJSON.length, nameArr = [];
-    while (length--) {
-      nameArr.push(storeJSON[length].name);
-    }
-    return nameArr;
-  };
-
-  this.names = getNames();
-
   this.displayProductsAndAddEvents = function () {
     displayNewProducts(storeJSON);
     var length = valuesToSort.length, store = this;
-    while (length--) {
-      //Each execution context has its variable scope and thus the length value is maintained.
-      (function (length) {
-        document.getElementById("value").addEventListener("change", function (event) {
-          sortProductsBy(event.target.value, store);
-        });
-      })(length);
-    }
+    document.getElementById("value").addEventListener("change", function (event) {
+      displayNewProducts(sortJSONBy(event.target.value));
+    });
   };
+
+  var sortJSONBy = function (attribute) {
+    var sortedJSON = storeJSON.sort(function(a,b) {
+      if (parseInt(storeJSON[1][attribute]) == storeJSON[1][attribute]) {
+        if (parseInt(a[attribute]) > parseInt(b[attribute])) return 1;
+        if (parseInt(a[attribute]) < parseInt(b[attribute])) return -1;
+        return 0
+      }else {
+        if (a[attribute] > b[attribute]) return 1;
+        if (a[attribute] < b[attribute]) return -1;
+        return 0
+      }
+    })
+    return sortedJSON;
+  }
 
   this.addOptionsToSelectBox = function () {
     var fragment = document.createDocumentFragment();
@@ -78,28 +56,12 @@ function Store() {
       node.removeChild(node.firstChild);
     }
   };
-
-  var sortProductsBy = function (attribute, store) {
-    var productJSON = [];
-    if (attribute == "name") productJSON = storeJSON;
-    else {
-      var sortedArr = (attribute) ? store[attribute].sort() : [], i = 0,j = 0;
-      for (j = 0; j < sortedArr.length; j++) {
-        for (i = 0; i < storeJSON.length; i++) {
-          if (sortedArr[j] == storeJSON[i][attribute]) {
-            productJSON.push(storeJSON[i]);
-          }
-        }
-      }
-    }
-    displayNewProducts(productJSON);
-  };
 }
 
 window.addEventListener("load", function () {
   "use strict";
   var store = new Store();
   store.addOptionsToSelectBox();
-  store.populateValuesOfArrributes();
+  // store.populateValuesOfArrributes();
   store.displayProductsAndAddEvents();
 });
