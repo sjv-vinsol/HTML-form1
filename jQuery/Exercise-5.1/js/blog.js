a9ff555d501bc76185728d0e500cba9ecc79177d
sanjeev <sanjeev@vinsol.com>
2013-10-24 16:15:51 +0530

    Remove unnecessary files and improve naming

--- jQuery/Exercise-5.1/js/blog.js
+++ jQuery/Exercise-5.1/js/blog.js
@@ -1,10 +1,10 @@
-var elemA = $( ".listText" );
-elemA.each(function(index, elem) {
+var blogList = $( ".listText" );
+blogList.each(function(index, elem) {
    $(this).click(function(event) {
     event.preventDefault();     //Prevent the link(anchor tag) default behaviour.
-    var elemP = $(this).closest("li").first().find("p.excerpt");
-    elemP.slideToggle(function(){
-      $("#blog p.excerpt").not(elemP).slideUp();     //Slide up any other opened para.
+    var descriptionPara = $(this).closest("li").first().find("p.excerpt");
+    descriptionPara.slideToggle(function(){
+      $("#blog p.excerpt").not(descriptionPara).slideUp();     //Callback function to slide up any other opened para.
     });
   })
 })
