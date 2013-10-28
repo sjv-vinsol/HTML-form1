cf1e0c5647c9b5d5a0528805c2927f3fe988c9c6
sanjeev <sanjeev@vinsol.com>
2013-10-28 15:05:11 +0530

    Implemented following changes :
    1. Optimize selector.
    2. removed first() method as it is not required.
    3. Used :visible filter to filter p.excerpt

--- jQuery/5.1 Reveal hidden text/js/blog.js
+++ jQuery/5.1 Reveal hidden text/js/blog.js
@@ -1,10 +1,9 @@
-var blogList = $( ".listText" );
+var blogList = $("#blog > ul").find("h3");
 blogList.each(function(index, elem) {
    $(this).click(function(event) {
     event.preventDefault();     //Prevent the link(anchor tag) default behaviour.
-    var descriptionPara = $(this).closest("li").first().find("p.excerpt");
-    descriptionPara.slideToggle(function(){
-      $("#blog p.excerpt").not(descriptionPara).slideUp();     //Callback function to slide up any other opened para.
-    });
+    var descriptionPara = $(this).siblings("p.excerpt");
+    descriptionPara.slideToggle();
+    $("#blog p.excerpt:visible").not(descriptionPara).slideUp();     //Callback function to slide up any other opened para.
   })
 })
