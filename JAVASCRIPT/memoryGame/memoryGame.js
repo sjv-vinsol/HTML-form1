47747580b1ca212b360907a313a75be7ac592ba0
sanjeev <sanjeev@vinsol.com>
2013-10-28 12:38:09 +0530

    Implemented below mentioned changes :
    1. Added delay of 2-3 seconds for incorrect match.
    2. Added function emptyGrid to remove all inner nodes.

--- JAVASCRIPT/memoryGame/memoryGame.js
+++ JAVASCRIPT/memoryGame/memoryGame.js
@@ -1,6 +1,9 @@
 var imageJSON = [ {url: "image_0.jpg"}, {url: "image_1.jpg"}, {url: "image_2.jpg"}, {url: "image_3.jpg"}, {url: "image_4.jpg"}, {url: "image_5.jpg"}, {url: "image_6.jpg"}, {url: "image_7.jpg"}, {url: "image_8.jpg"}, {url: "image_9.jpg"}, {url: "image_10.jpg"}, {url: "image_11.jpg"}, {url: "image_12.jpg"}, {url: "image_13.jpg"}, {url: "image_14.jpg"}, {url: "image_15.jpg"}, {url: "image_16.jpg"}, {url: "image_17.jpg"}, {url: "image_0.jpg"}, {url: "image_1.jpg"}, {url: "image_2.jpg"}, {url: "image_3.jpg"}, {url: "image_4.jpg"}, {url: "image_5.jpg"}, {url: "image_6.jpg"}, {url: "image_7.jpg"}, {url: "image_8.jpg"}, {url: "image_9.jpg"}, {url: "image_10.jpg"}, {url: "image_11.jpg"}, {url: "image_12.jpg"}, {url: "image_13.jpg"}, {url: "image_14.jpg"}, {url: "image_15.jpg"}, {url: "image_16.jpg"}, {url: "image_17.jpg"}];
 
 function Game() {
+  this.delayForNoMatch = false;
+  this.delayCountInSec = 2;
+  this.imageBoxCount = 36;
   var count = 0;
   var totalCount = 0;
   var previousClickImg = "";
@@ -26,64 +29,81 @@ function Game() {
   }
 
   this.start = function() {
+    var game = this;
     fragment = document.createDocumentFragment();
-    for (i=0;i<36;i++) {
+    for (i = 0; i < this.imageBoxCount; i++) {
       div = fragment.appendChild(document.createElement("div"));
       div.classList.add("imageBox");
       imgElem = div.appendChild(document.createElement("img"));
       addImagesToImageBox(imgElem, i);
-      div.addEventListener("click", logicOnImageClick);
+      div.addEventListener("click", function(event) {
+        logicOnImageClick(event, game, this);
+      })
     }
     document.getElementById("grid").appendChild(fragment);
   }
 
   this.startTimer = function () {
     var timerCount = 0;
-    timerHandler = setInterval(function() {
+    this.timerHandler = setInterval(function() {
       timerCount++;
       document.getElementById("timer").innerHTML = "Time Elapsed : &nbsp&nbsp" + timerCount;
     }, 1000);
   }
 
-  function logicOnImageClick(event) {
-    if (event.target.tagName != "IMG") {
+  function logicOnImageClick(event, game, currentClickedElem) {
+    if (event.target.tagName != "IMG" && !game.delayForNoMatch) {
       count++;
-      var elemClickImg = this.getElementsByTagName("img")[0];
+      var elemClickImg = currentClickedElem.getElementsByTagName("img")[0];
       elemClickImg.classList.remove("hidden");
-      if (count == 1) previousClickImg = this.getElementsByTagName("img")[0];
+      if (count == 1) previousClickImg = currentClickedElem.getElementsByTagName("img")[0];
       else if (count == 2) {
         if (elemClickImg.src == previousClickImg.src) {
           totalCount += 2;
         }
         else {
+          game.delayForNoMatch = true;
           setTimeout(function() {
             previousClickImg.classList.add("hidden");
             elemClickImg.classList.add("hidden");
+            game.delayForNoMatch = false;
           }, 200);
         }
         count = 0;
       }
     }
-    if(totalCount == 36) {
-      clearInterval(timerHandler);
+    if(totalCount == game.imageBoxCount) {
+      clearInterval(game.timerHandler);
       document.getElementById("playAgain").classList.add('redBackground');
     }
   }
+}
+
+function startNewGame() {
+  game = new Game();
+  game.shuffleImages();
+  game.startTimer();
+  game.start();
 }
 
 (function main() {
   document.getElementById("start").addEventListener("click", function() {
     document.getElementById("start").classList.add('hidden');
     document.getElementById('gameContainer').classList.remove('hidden');
-    var game = new Game();
-    game.shuffleImages();
-    game.startTimer();
-    game.start();
+    startNewGame();
   })
 })();
 
 document.getElementById("playAgain").addEventListener("click", function() {
   if (confirm("Are you sure want to play again")) {
-    location.reload();
+    clearInterval(game.timerHandler);
+    emptyGrid(document.getElementById('grid'));
+    startNewGame();
   }
 });
+
+function emptyGrid(elem) {
+  while (elem.firstChild) {
+    elem.removeChild(elem.firstChild);
+  }
+}
