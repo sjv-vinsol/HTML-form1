44d4c79adb4529c76a25c40cda9f55e3a3eb374a
sanjeev <sanjeev@vinsol.com>
2013-10-30 12:16:14 +0530

    Removed some extra lines

--- jQuery/5.2 Create Drop down menus/js/navigation.js
+++ jQuery/5.2 Create Drop down menus/js/navigation.js
@@ -1,11 +1,7 @@
 $(document).ready(function() {
   $.fn.hover = function(event) {
-    if (event.type == "mouseenter") {
-      this.children("ul").addClass("hover");
-    }
-    else {
-      this.children("ul").removeClass("hover");
-    }
+    if (event.type == "mouseenter") this.children("ul").addClass("hover");
+    else this.children("ul").removeClass("hover");
   }
 
   $('#nav').children('li').bind("mouseenter mouseleave", function(event){
