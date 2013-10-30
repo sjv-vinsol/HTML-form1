8424f9292c4a4cbabf16e57e08fce91b5c5ee0b6
sanjeev <sanjeev@vinsol.com>
2013-10-30 13:47:36 +0530

    removed variable descriptionPara and changed slideToggle to slideDown

--- jQuery/5.1 Reveal hidden text/js/blog.js
+++ jQuery/5.1 Reveal hidden text/js/blog.js
@@ -2,5 +2,5 @@ var $blogList = $("#blog > ul").find("h3");
 $blogList.click(function(event) {
     event.preventDefault();     //Prevent the link(anchor tag) default behaviour.
     $("#blog p.excerpt:visible").slideUp();     //Callback function to slide up any other opened para.
-    var descriptionPara = $(this).siblings("p.excerpt:hidden").slideToggle();
+    $(this).siblings("p.excerpt:hidden").slideDown();
 })
