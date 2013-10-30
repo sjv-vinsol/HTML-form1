1a1907a648f5e1a78b6d22c83d76e09c138692f8
sanjeev <sanjeev@vinsol.com>
2013-10-30 13:54:07 +0530

    Changed function name from hover to onHoverElem

--- jQuery/5.2 Create Drop down menus/js/navigation.js
+++ jQuery/5.2 Create Drop down menus/js/navigation.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-  $.fn.hover = function(event) {
+  $.fn.onElemHover = function(event) {
     if (event.type == "mouseenter") this.children("ul").addClass("hover");
     else this.children("ul").removeClass("hover");
   }
 
   $('#nav').children('li').bind("mouseenter mouseleave", function(event){
-    $(this).hover(event);
+    $(this).onElemHover(event);
   });
 });
