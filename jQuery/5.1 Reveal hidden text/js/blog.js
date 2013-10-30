56aad14fcf2bafb90fb1ebb1d8efc8bf19bd7d4f
sanjeev <sanjeev@vinsol.com>
2013-10-30 12:01:16 +0530

    Implemented following changes :
    1. Used $ before variable name if it stores jQuery object.
    2. .not() funciton is removed and used :hidden instead with some change in
    logic.
    3. Removed the loop to add click event as jQuery will itself iterate over
    the array to add the click event.

--- jQuery/5.1 Reveal hidden text/js/blog.js
+++ jQuery/5.1 Reveal hidden text/js/blog.js
@@ -1,9 +1,6 @@
-var blogList = $("#blog > ul").find("h3");
-blogList.each(function(index, elem) {
-   $(this).click(function(event) {
+var $blogList = $("#blog > ul").find("h3");
+$blogList.click(function(event) {
     event.preventDefault();     //Prevent the link(anchor tag) default behaviour.
-    var descriptionPara = $(this).siblings("p.excerpt");
-    descriptionPara.slideToggle();
-    $("#blog p.excerpt:visible").not(descriptionPara).slideUp();     //Callback function to slide up any other opened para.
-  })
+    $("#blog p.excerpt:visible").slideUp();     //Callback function to slide up any other opened para.
+    var descriptionPara = $(this).siblings("p.excerpt:hidden").slideToggle();
 })
