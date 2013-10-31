1531831f80da357b530cf31c50cd115e8f2b7efa
sanjeev <sanjeev@vinsol.com>
2013-10-31 11:11:29 +0530

    Added comment on top of file

--- jQuery/6.1 Load External Content/js/load.js
+++ jQuery/6.1 Load External Content/js/load.js
@@ -1,6 +1,10 @@
+// For security reasons, on Chrome and Opera you cannot use XMLHttpRequest to load local files.
+// You must run it on a web server, even if that web server is running on localhost.
+// The above flag will allow chrome to run file:// url also.
+// Below code will work fine on production wiht http:// url.
 var pathname = "data/blog.html";
 var $headlines = $( '#blog' ).find("h3").after($( '<div/>', {class: "blogPost"} )).find("a");
-referenceIdArr = ["post1", "post2", "post3" ];       // THese are the ID which are present in the reference file.
+referenceIdArr = ["post1", "post2", "post3" ];       // These are the ID which are present in the reference file.
 
 $.each($( '#blog' ).find("h3"), function(index, data){
   $(this).data("referenceId", referenceIdArr[index]);
