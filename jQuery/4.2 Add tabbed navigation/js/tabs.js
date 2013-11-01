26c91dee35c240ed1a53001730c03a652abfe053
sanjeev <sanjeev@vinsol.com>
2013-11-01 14:24:44 +0530

    Provide proper spacing

--- jQuery/4.2 Add tabbed navigation/js/tabs.js
+++ jQuery/4.2 Add tabbed navigation/js/tabs.js
@@ -1,18 +1,18 @@
-var $moduleElem = $(".module").addClass("hidden");
-var $moduleList = $("<ul/>", {id: "moduleList"}).insertBefore($moduleElem.first());
+var $moduleElem = $( ".module" ).addClass( "hidden" );
+var $moduleList = $( "<ul/>", {id: "moduleList"} ).insertBefore($moduleElem.first());
 
 $.each($moduleElem, function() {
-    $contentOfUl = $(this).children().hide();
-    $h2Elem = $contentOfUl.filter("h2");
-    var h2ElemText = $h2Elem.text();
-    $liElem = $("<li/>", {id: "new"+h2ElemText, class: "ulList", text: h2ElemText}).appendTo($moduleList).append($h2Elem.siblings());
-    $liElem.bind("click", function(event){
-        var that = this;
-        (function handler(h2ElemText) {
-          if ($(event.target).attr("id") == ("new" + h2ElemText)) {
-            $(that).siblings().removeClass('current grey').children().hide();
-            $(that).toggleClass('current grey').children().toggle();
-          }
-        })(h2ElemText);
-    })
+  $contentOfUl = $(this).children().hide();
+  $h2Elem = $contentOfUl.filter( "h2" );
+  var h2ElemText = $h2Elem.text();
+  $liElem = $( "<li/>", {id: "new" + h2ElemText, class: "ulList", text: h2ElemText} ).appendTo($moduleList).append($h2Elem.siblings());
+  $liElem.bind( "click", function(event){
+    var that = this;
+    (function handler(h2ElemText) {
+      if ($(event.target).attr( "id" ) == ("new" + h2ElemText)) {
+        $(that).siblings().removeClass( 'current grey' ).children().hide();
+        $(that).toggleClass( 'current grey' ).children().toggle();
+      }
+    })(h2ElemText);
+  })
 })
