870dc55f18f12fdb4282f52efaa6fcf4de5c4534
sanjeev <sanjeev@vinsol.com>
2013-11-13 20:20:02 +0530

    Used is() jQuery method

--- jQuery/7 .live() exercise/js/live.js
+++ jQuery/7 .live() exercise/js/live.js
@@ -1,15 +1,15 @@
-var $divContainer = $(' <div/> ', {id: "divContainer"}), id = 0;
+var $divContainer = $(' <div/> ', {id: "divContainer"}), divNumber = 0;
 var $addButton = $(' <input/> ', {id: "addButton", value: "Add Item", type: "button"});
 
-$( 'body' ).prepend($addButton).prepend( $divContainer ).delegate( 'input#addButton', "click", function (event){
-    $divContainer.append($( "<div/>" , {id: (id++), class: "innerDiv grey"}));
+$( 'body' ).append($addButton).append( $divContainer ).delegate( 'input#addButton', "click", function (event){
+    $divContainer.append($( "<div/>" , {text: (++divNumber), class: "innerDiv grey"}));
 })
   .delegate( 'div.innerDiv', "click", function (event){
     var $clickedDiv = $(event.target), $innerDivs = $divContainer.children( ".innerDiv" );
-    if ($clickedDiv.attr("id") == $innerDivs.last().attr("id")) {
-      $innerDivs.last().remove();
+    if ($clickedDiv.is($innerDivs.last())) {
+      $clickedDiv.remove();
     }else {
-      $innerDivs.removeClass( "heighlight" );
       $clickedDiv.addClass( "heighlight" );
+      $clickedDiv.siblings().removeClass( "heighlight" );
     }
   })
