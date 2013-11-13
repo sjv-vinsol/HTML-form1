c04034172503a13b27aaac0fb88b80a523634ef3
sanjeev <sanjeev@vinsol.com>
2013-11-13 17:32:45 +0530

    Removed unnecessary files
    1. Search is performed by Id and not by text
    2. Highlighted the div with grey color.

--- jQuery/7 .live() exercise/js/live.js
+++ jQuery/7 .live() exercise/js/live.js
@@ -1,13 +1,12 @@
-var $divContainer = $(' <div/> ', {id: "divContainer"}), noOfDivs = 0;
+var $divContainer = $(' <div/> ', {id: "divContainer"}), id = 0;
 var $addButton = $(' <input/> ', {id: "addButton", value: "Add Item", type: "button"});
 
 $( 'body' ).prepend($addButton).prepend( $divContainer ).delegate( 'input#addButton', "click", function (event){
-    $divContainer.append($( "<div/>" , {text: ++noOfDivs, class: "innerDiv"})); 
+    $divContainer.append($( "<div/>" , {id: (id++), class: "innerDiv grey"}));
 })
   .delegate( 'div.innerDiv', "click", function (event){
     var $clickedDiv = $(event.target), $innerDivs = $divContainer.children( ".innerDiv" );
-    if ($clickedDiv.text() == $innerDivs.last().text()) {
-      noOfDivs--;
+    if ($clickedDiv.attr("id") == $innerDivs.last().attr("id")) {
       $innerDivs.last().remove();
     }else {
       $innerDivs.removeClass( "heighlight" );
