fb3b530526fadd39dca44b79c139bc1d2f318a92
sanjeev <sanjeev@vinsol.com>
2013-11-14 12:01:57 +0530

    Implemented below mentioned changes :
    1. Used object oriented approach.
    2. On click of product name, changed the header selection only if there is
    a radio button already selected.

--- jQuery/Product rating (Level 2)/js/productRating.js
+++ jQuery/Product rating (Level 2)/js/productRating.js
@@ -1,52 +1,76 @@
 $(document).ready(function () {
 
-  var main = {
-    productNames: ["Coffee", "tea", "Sodas", "Cake", "Drinks"].sort().reverse(),
-    selectedHeader: {},
-    selectedProduct: {},
-    displayProducts: function () {
-      var trElem = {}, length = main.productNames.length;
+  function ProductRating() {
+    this.productNames = ["Coffee", "tea", "Sodas", "Cake", "Drinks", "Pizza"].sort().reverse();
+    this.selectedHeader = {};
+    this.selectedProduct = {};
+
+    this.displayProducts = function () {
+      var trElem = {}, length = this.productNames.length;
       while (length--) {
         trElem = $( "#rowTemplate" ).clone();
-        trElem.find( ".radio" ).attr({name: main.productNames[length]});
-        trElem.children( ".product" ).text(main.productNames[length]);
+        trElem.find( ".radio" ).attr({name: this.productNames[length]});
+        trElem.children( ".product" ).text(this.productNames[length]);
         trElem.appendTo($( "#productRating" ));
       };
-      main.products = $( ".product" );
-      main.headers = $( "#productRating th" );
-      main.radioButtons = $( ".radio" );
-    }
+      this.populateData();
+    };
+
+    this.populateData = function () {
+      this.products = $( ".product" );
+      this.headers = $( "#productRating th" );
+      this.radioButtons = $( ".radio" );
+      this.productsOnClick();
+      this.headersOnClick();
+      this.radioButtonsOnClick();
+    };
+
+    function handlerForHeaderClick (productRating) {
+      var index = 0;
+      productRating.headers.removeClass( "selected" );
+      productRating.selectedHeader.addClass( "selected" );
+      if (productRating.selectedProduct[0]) {
+        index = productRating.selectedHeader.index();
+        productRating.selectedProduct.parent().find( "td" ).eq(index).find( "input" ).trigger( "click" );
+      }
+    };
+
+    this.productsOnClick = function () {
+      var productRating = this;
+      this.products.bind("click", function (event) {
+        var checkedRadio = $(this).parent("#rowTemplate").find(".radio:checked");
+        productRating.products.removeClass( 'selected' );
+        productRating.selectedProduct = $(this).addClass( "selected" );     
+        if (checkedRadio[0]) {
+          productRating.headers.eq(checkedRadio.parent("td").index()).trigger("click");
+        } else {
+          if (!$.isEmptyObject(productRating.selectedHeader )) productRating.selectedHeader.trigger("click");
+        }
+      });
+    };
+
+    this.headersOnClick = function () {
+      var productRating = this;
+      this.headers.not("#blank").bind("click", function () {
+        productRating.selectedHeader = $(this);
+        handlerForHeaderClick(productRating);
+      });
+    };
+       
+    this.radioButtonsOnClick = function () {
+      var productRating = this;
+      this.radioButtons.bind("click", function () {
+        var radioButton = $(this);
+        productRating.headers.removeClass( "selected" );
+        productRating.products.removeClass( "selected" );
+        productRating.selectedHeader = productRating.headers.eq(radioButton.closest( 'td' ).index()).addClass( "selected" );
+        productRating.selectedProduct = radioButton.closest( "tr" ).find( ".product" ).addClass( "selected" );
+      });
+    };
   };
 
-  main.displayProducts();
-
-  function handlerForHeaderClick() {
-    var index = 0;
-    main.headers.removeClass( "selected" );
-    main.selectedHeader.addClass( "selected" );
-    if (main.selectedProduct[0]) {
-      index = main.selectedHeader.index();
-      main.selectedProduct.parent().find( "td" ).eq(index).find( "input" ).trigger( "click" );
-    }
-  };
-
-  main.products.bind("click", function () {
-    main.products.removeClass( 'selected' );
-    main.selectedProduct = $(this).addClass( "selected" );
-  });
-
-  main.headers.bind("click", function () {
-    main.selectedHeader = $(this);
-    if (main.selectedHeader[0] != main.headers[0]) {
-      handlerForHeaderClick();
-    }
-  });
-
-  main.radioButtons.bind("click", function () {
-    var radioButton = $(this);
-    main.headers.removeClass( "selected" );
-    main.products.removeClass( "selected" );
-    main.selectedHeader = main.headers.eq(radioButton.closest( 'td' ).index()).addClass( "selected" );
-    main.selectedProduct = radioButton.closest( "tr" ).find( ".product" ).addClass( "selected" );
-  });
+  (function productRating() {
+    var productRating = new ProductRating();
+    productRating.displayProducts();
+  })();
 });
