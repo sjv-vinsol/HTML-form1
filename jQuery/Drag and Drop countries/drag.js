84c58fb922e85c01adb71c7294e95a875cd2f138
sanjeev <sanjeev@vinsol.com>
2013-11-15 15:41:24 +0530

    Implemented below mentioned changes :
    1. Extract if condition to separate funciton
    2. Maintain sorted countries after each drop

--- jQuery/Drag and Drop countries/drag.js
+++ jQuery/Drag and Drop countries/drag.js
@@ -1,14 +1,27 @@
 $(document).ready(function () {
   var countriesJSON = { leftCountries: ["India", "China", "Nepal", "Sri Lanka"], rightCountries: ["Usa", "Australia", "Quatar", "New Zealand"]};
 
-  function appendCountriesToSelecBox(idOfCountryContainer) {
+  function appendCountriesToSelecBox (idOfCountryContainer) {
     var containerElem = $("#"+ idOfCountryContainer +"");
+    containerElem.html("");
     var soretedCountryArr = countriesJSON[idOfCountryContainer].sort();
     var length = countriesJSON[idOfCountryContainer].length, optionElem = {};
     while (length--) {
-      optionElem = $('<p/>', {class: "country"}).text(soretedCountryArr[length]);
+      optionElem = $('<p/>', {class: "country", id: soretedCountryArr[length]}).text(soretedCountryArr[length]);
       containerElem.prepend(optionElem);
     }
+  }
+
+  function sortJqueryObjectBy(attr, obj) {
+    obj.sort(function(a, b) {
+      return (a.getAttribute(attr) > b.getAttribute(attr));
+    })
+    return obj;
+  }
+
+  function onDifferentCountrySection(draggedElem, countrySection) {
+    if (!countrySection.is(draggedElem.parent())) return true
+      else return false;
   }
 
   appendCountriesToSelecBox("leftCountries");
@@ -18,7 +31,7 @@ $(document).ready(function () {
   $('.selectBox').droppable({
     accept: ".country",
     over: function (event, elemObj) {
-      if ($(this)[0] != elemObj.draggable.parent()[0]) {
+      if ( onDifferentCountrySection(elemObj.draggable, $(this))) {
         elemObj.draggable.addClass("yellow");
       }
     },
@@ -34,8 +47,11 @@ $(document).ready(function () {
     drop: function (event, elemObj) {
       var draggedElem = elemObj.draggable, droppableElem = $(this);
       draggedElem.removeClass("yellow");
-      if (droppableElem[0] != draggedElem.parent()[0]) {
-        droppableElem.append(draggedElem);
+      if (onDifferentCountrySection(draggedElem, droppableElem)) {
+        var idOfCountryContainer = droppableElem.attr("id");
+        // sort the children in droppableElem by Id
+        var sortedElems = sortJqueryObjectBy("id", droppableElem.children().detach().add(draggedElem.detach()))
+        droppableElem.append(sortedElems);
       }
     }
   });
