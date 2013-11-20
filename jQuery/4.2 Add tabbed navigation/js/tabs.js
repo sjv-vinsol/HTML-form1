052051e9e0fabc0da9bb60663a7ab37ad9af49d7
sjv-vinsol <sanjeev@vinsol.com>
2013-11-20 15:32:35 +0530

    hide/show the module on click of li elem

--- jQuery/4.2 Add tabbed navigation/js/tabs.js
+++ jQuery/4.2 Add tabbed navigation/js/tabs.js
@@ -1,18 +1,13 @@
-var $moduleElem = $( ".module" ).addClass( "hidden" );
+var $moduleElem = $( ".module" ).hide();
 var $moduleList = $( "<ul/>", {id: "moduleList"} ).insertBefore($moduleElem.first());
 
 $.each($moduleElem, function() {
-  $contentOfUl = $(this).children().hide();
-  $h2Elem = $contentOfUl.filter( "h2" );
-  var h2ElemText = $h2Elem.text();
-  $liElem = $( "<li/>", {id: "new" + h2ElemText, class: "ulList", text: h2ElemText} ).appendTo($moduleList).append($h2Elem.siblings());
-  $liElem.bind( "click", function(event){
-    var that = this;
-    (function handler(h2ElemText) {
-      if ($(event.target).attr( "id" ) == ("new" + h2ElemText)) {
-        $(that).siblings().removeClass( 'current grey' ).children().hide();
-        $(that).toggleClass( 'current grey' ).children().toggle();
-      }
-    })(h2ElemText);
-  })
+  var h2ElemText = $(this).children("h2").text();
+  $liElem = $( "<li/>", {id: "new" + h2ElemText, class: "ulList", text: h2ElemText} ).appendTo($moduleList);
+  (function bindClickEvent ($liElem) {
+    $liElem.on( "click", function(event){
+      $moduleElem.filter(':visible').hide();
+      $moduleElem.eq($liElem.index()).show();
+    })
+  })($liElem);
 })
