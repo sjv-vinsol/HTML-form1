1d02ed84c036a48b219d263931baa91dd7e56458
sjv-vinsol <sanjeev@vinsol.com>
2013-11-20 16:43:48 +0530

    Ajax will be send only once and stored response is afterwards

--- jQuery/6.2 Load content using JSON/js/specials.js
+++ jQuery/6.2 Load content using JSON/js/specials.js
@@ -1,20 +1,29 @@
 var $displaySpecialDiv = $("#specialContent").removeClass("hidden");
 $("#specials").find("form").after($displaySpecialDiv);
 
-$.ajax({
-  url: "data/specials.json",
-  dataType: "json",
-  success: function(jsonResponse) {
-    $("#specials form select").bind("change", function(){
-      var val = $(this).val();
-        var specialDayData = jsonResponse[val];
-        if (specialDayData) {
-          $("#title").html(specialDayData["title"]);
-          $("#text").html(specialDayData["text"]);
-          $("#image").attr("src", specialDayData["image"]);
-          $("#color").html(specialDayData["color"]);
-        }
+function displaySpecials(specialData) {
+  console.log("Special Data  :   ", specialData);
+  $("#title").html(specialData["title"]);
+  $("#text").html(specialData["text"]);
+  $("#image").attr("src", specialData["image"]);
+  $("#color").html(specialData["color"]);
+}
+var jsonFetched = false, response = {};
+
+$("#specials form select").on("change", function(){
+  var day = $(this).val();
+  if (!jsonFetched) {
+    $.ajax({
+      url: "data/specials.json",
+      dataType: "json",
+      success: function(jsonResponse) {
+        response = jsonResponse;
+        jsonFetched = true;
+        displaySpecials(response[day]);
+      }
     })
+  } else {
+    if (day) displaySpecials(response[day]);
   }
 })
 $("#specials form li.buttons").remove();
