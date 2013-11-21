406a3b66f2058c6f7145ebc390b8b40119ef1378
sjv-vinsol <sanjeev@vinsol.com>
2013-11-21 19:28:40 +0530

    Calculated the index of next elem first and then fetches the element

--- jQuery/5.3 Create a Slideshow/js/slideshow.js
+++ jQuery/5.3 Create a Slideshow/js/slideshow.js
@@ -1,22 +1,22 @@
 var $slideshow = $("#slideshow").children("li").addClass("hidden"), navArea = $('<div/>', {id: "navigation"});
-var displaySlideshowDiv = $('<div/>').append($slideshow), navigationDelay = 500, displayTime = 3000;
+var displaySlideshowDiv = $('<div/>').append($slideshow), navigationDelay = 200, displayTime = 1000;
 $("body").prepend(displaySlideshowDiv);
 
 // add image numbers to navigation area
 for (i = 0; i < $slideshow.length; i++) {
   var elem = navArea.append($('<span/>', {id: i + 1, text: i + 1, class: "navigation"}));
 }
-var $navigationNumbers = navArea.insertAfter(displaySlideshowDiv).children("span");
+var $navigationNumbers = navArea.insertAfter(displaySlideshowDiv).children( "span" );
 $slideshow.first().fadeIn(navigationDelay);
-$navigationNumbers.first().addClass("redBackground");
+$navigationNumbers.first().addClass( "redBackground" );
 var indexOfLastElem = $slideshow.last().index();
 setInterval(function(){
-  var $visibleElem = $slideshow.filter(":visible");
+  var $visibleElem = $slideshow.filter( ":visible" );
   var indexOfVisibleElem = $visibleElem.index();
   $visibleElem.fadeOut(navigationDelay, function(){
-    $($navigationNumbers[indexOfVisibleElem]).removeClass("redBackground");
-    var $nextElem = $((indexOfVisibleElem == indexOfLastElem) ? $slideshow.first() : $slideshow.get(indexOfVisibleElem + 1));
-    $($navigationNumbers.get($nextElem.index())).addClass("redBackground");
-    $nextElem.fadeIn(navigationDelay);
+    $($navigationNumbers[indexOfVisibleElem]).removeClass( "redBackground" ); 
+    var indexOfNextElem = (indexOfVisibleElem == indexOfLastElem) ? 0 : (indexOfVisibleElem + 1);
+    $navigationNumbers.eq(indexOfNextElem).addClass( "redBackground" );
+    $slideshow.eq(indexOfNextElem).fadeIn(navigationDelay);
   })
 }, displayTime);
