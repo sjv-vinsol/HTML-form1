f87d0da809c1c6363e17ec39d5d03ed650101a47
sjv-vinsol <sanjeev@vinsol.com>
2013-11-25 11:50:28 +0530

    Implemented below mentioned changes :
    1. Added a initialize method to initialize the cart.
    2. Moved methods inside cart that belongs to cart and are previously present outside the class.
    3. Added some methods to cart prototype.
    4. Used ajax to fetch productJSON.
    5. Break long methods to short and more readable methods.

--- jQuery/jQuery Shopping Cart/js/cart.js
+++ jQuery/jQuery Shopping Cart/js/cart.js
@@ -1,163 +1,229 @@
-var productJSON = {};
 $(document).ready(function () {
-  $( "#productTab" ).bind("click", function () {
-    $( ".tab" ).removeClass( "selected" );
-    $(this).addClass( "selected" );
-    $( "#myCart" ).addClass( "hidden" );
-    $( '#product' ).removeClass( "hidden" );
-  });
-
-  // Display myCart section on click of "My Cart" tab in navigation bar
-  $( "#myCartTab" ).bind("click", function () {
-    $( ".tab" ).removeClass( "selected" );
-    $(this).addClass( "selected" );
-    $( "#product" ).addClass( "hidden" );
-    $( '#myCart' ).removeClass( "hidden" );
-  });
-  $( "#productTab" ).trigger( "click" );
-
-
-  function displayCart() {
-    var cart = new Cart();
-    cart.displayProducts();
-  };
-
-  // gather products from external file.
-  $.ajax({
-    url: "data/products.json",
-    dataType: "json",
-    success: function(response) {
-      productJSON = response;
-      displayCart();
-    }
-  })
-})
+  var cart = new Cart();
+  cart.fetchDataAndInitializeCart();
+});
 
 // Create class Product
-function Product(productNumber) {
+function Product(jsonValue, productNumber) {
   this.quantity = 0;
-  this.imgSrc = "images/" + productJSON[productNumber].imgName;
+  this.imgSrc = "images/" + jsonValue.imgName;
   this.number = productNumber;
-  this.name = productJSON[productNumber].name;
-  this.category = productJSON[productNumber].category;
-  this.description = productJSON[productNumber].description;
-  this.price = productJSON[productNumber].price;
+  this.name = jsonValue.name;
+  this.category = jsonValue.category;
+  this.description = jsonValue.description;
+  this.price = jsonValue.price;
   this.subtotal = 0;
-}
+};
 
 // Create class Cart
 function Cart() {
   this.myCartList = {};
   this.total = 0;
-  this.totalQtyInCart = 0;
+  this.products = {};
+  this.productJSON = {};
 
-  this.updateMyCartCount = function () {
-    $( '#myCartTab'  ).text("My Cart ("+this.totalQtyInCart+")");
-  };
-
-  this.updateOrderTotalAndCount = function () {
+  this.fetchDataAndInitializeCart = function () {
     var cart = this;
-    cart.total = 0; cart.totalQtyInCart = 0;
-    $.each(this.myCartList, function (key, product) {
-      cart.total += product.subtotal;
-      cart.totalQtyInCart += product.quantity;
-    });
-    $( '#total' ).text("Total : "+cart.total+"");
-    this.updateMyCartCount();
-  };
-
-  // On change in quantity input
-  this.qtyOnChange = function ($qtyElem, product) {
-    var cart = this;
-    $qtyElem.bind("keyup", function () {
-      var qty = $(this).val();
-      product.quantity = (qty == "") ? 0 : parseInt(qty, 10);
-      product.subtotal = product.quantity * product.price;
-      cart.updateOrderTotalAndCount();
-      updateMyCartDetais(product);
-    });
-  };
-
-  this.addProductDetailsTo = function ($trElem, product, isMyCartSection) {
-    var cart = this;
-    $trElem.find( ".name" ).text(product.name);
-    if (isMyCartSection) {
-      var $qtyElem = $trElem.find( ".qtyInput" );
-      $trElem.find( ".cartPrice" ).text(product.price);
-      $qtyElem.val(product.quantity);
-      this.qtyOnChange($qtyElem, product);
-    } else {
-      $trElem.find( ".category" ).text( "Category: "+product.category+"" );
-      $trElem.find( ".description" ).text( "Description: "+product.description+"" );
-      $trElem.find( ".price" ).text( "Price: "+product.price+"" );
-    };
-  };
-
-  function removeElemHandler(product, $myCartTrElem, cart) {
-    delete cart.myCartList[product.number];
-    product.quantity = 0;
-    cart.updateOrderTotalAndCount();
-    $myCartTrElem.remove();
-  };
-
-  function updateMyCartDetais(product) {
-    var $elem = $( "#"+product.number+"" );
-    $elem.find( ".subTotal" ).text(product.subtotal);
-    (product.quantity) ? $elem.find( ".qtyInput" ).val(product.quantity) : "";
-  };
-
-  this.updateProductAttributes = function (product, qty) {
-    product.quantity = (qty) ? qty : product.quantity;
-    product.subtotal = product.quantity * product.price;
-  };
-
-  this.addButtonOnClick = function (product, $productElem) {
-    var cart = this, $addToCartElem = $productElem.find( '.addToCart' );
-    $addToCartElem.bind("click", function () {
-      var qty = parseInt($productElem.find( '.qtyInput' ).val(), 10);
-      if (qty) {
-        if (cart.myCartList.hasOwnProperty(product.number)) {
-          qty += product.quantity;
-          cart.updateProductAttributes(product, qty);
-          cart.myCartList[product.number] = product;
-          updateMyCartDetais(product);
-          cart.updateOrderTotalAndCount();
-        } else {
-          cart.updateProductAttributes(product, qty);
-          cart.myCartList[product.number] = product;
-          var $myCartTrElem = $( '#cartTemplate' ).clone().removeClass( 'hidden' );
-          $myCartTrElem.find( ".productImg" ).attr({src: product.imgSrc});
-          $myCartTrElem.attr({id: product.number}).find( ".subTotal" ).text(product.subtotal);
-          $( '#myCart' ).append($myCartTrElem);
-          // Add details to myCart Elem
-          cart.addProductDetailsTo($myCartTrElem, product, true);
-          cart.updateOrderTotalAndCount();
-          $myCartTrElem.find( ".remove" ).bind("click", function () {
-            if (confirm("Are you sure want to remove "+product.name+" from Cart!!")) {
-              removeElemHandler(product, $myCartTrElem, cart);
-            }
-          });
-        }
-        alert("Product : "+product.name+" added/updated in Cart!!");
+    $.ajax({
+      url: "data/products.json",
+      dataType: "json",
+      success: function(response) {
+        cart.productJSON = response;
+        cart.init();
       }
+    })
+  };
+
+  this.init = function () {
+    this.displayProducts();
+    this.storeJqueryObjects();
+    this.bindEvents();
+    this.showDefaultList();
+  };
+
+  this.storeJqueryObjects = function () {
+    this.productTab = $( '#productTab' );
+    this.myCartTab = $( '#myCartTab' );
+    this.productListSectionDiv = $( '#allProductList' );
+    this.myCartProductsDiv = $( "#myCart" );
+    this.productRecords = $( ".productRecord" ).not( "#productTemplate" );
+  };
+
+  this.bindEvents = function () {
+    var cart = this;
+    this.productTab.on('click', function() {
+      cart.showProductList();
+    });
+    this.myCartTab.on('click', function() {
+      cart.displayProductInMyCart();
+    });
+    $('body').delegate(".addToCart", "click", function() {
+      cart.addToCart(this);
+    });
+    $("#search").on("click", function(){
+      cart.searchProductsByCategory($(this));
+    });
+    $("#show_all_category").on("click", function () {
+      cart.showAllProducts();
     });
   };
 
-  this.displayProduct = function (product) {
-    var $productElem = $( '#productTemplate' ).clone().removeClass( 'hidden' );
-    $productElem.find( ".productImg" ).attr({src: product.imgSrc});
-    // Third paramenter "false" is for "isMyCartSection" ie whether details are added to myCart(true) OR product(false)
-    this.addProductDetailsTo($productElem, product, false);
-    $( '#product' ).append($productElem.attr({id: "product"+product.number+""}));
-    this.addButtonOnClick(product, $productElem);
+  this.addToCart = function (addToCartButton) {
+    var $productElem = $(addToCartButton).closest('tr'), product = this.getProduct($productElem.attr("id"));
+    var qty = parseInt($productElem.find('.qtyInput').val(), 10);
+    if (qty) {
+      if (this.isPresentInCart($productElem.attr('id'))) {
+        this.updateCart(qty, product);
+      }else {
+        product.quantity = qty;
+        product.subTotal = qty * product.price;
+        this.addProductToCart(product);
+      }
+      alert("Product " + product.name + " successfully added/updated to cart!!!");
+    }
+  };
+
+  this.updateCart = function (qty, product) {
+    product.quantity += qty;
+    product.subTotal = product.price * product.quantity;
+    this.myCartList[product.number] = product.quantity;
+    $cartProduct = $("#cart_product" + product.number);
+    this.updateProductAttributes(product, $cartProduct, true);
+    this.updateTotalAndCartCount();
   };
   
+  this.addProductToCart = function(product) {    
+    var $myCartTrElem = $( '#cartTemplate' ).clone().removeClass( 'hidden' ).attr("id", "cart_product" + product.number);
+    $myCartTrElem.find( ".productImg" ).attr({src: product.imgSrc});
+    this.myCartProductsDiv.append($myCartTrElem);
+    this.bindEventOnRemoveButton($myCartTrElem, product);
+    this.bindOnChangeEventOnQty($myCartTrElem, product);
+    this.myCartList[product.number] = product.quantity;
+    this.updateProductAttributes(product, $myCartTrElem, true);
+    this.updateTotalAndCartCount();
+  };
+
+  this.bindOnChangeEventOnQty = function ($cartProduct, product) {
+    var cart = this;
+    $cartProduct.find(".qtyInput").on("change", function () {
+      var qty = parseInt($(this).val(), 10);
+      if (qty) {
+        product.quantity = qty;
+        cart.myCartList[product.number] = qty;
+        product.subTotal = product.quantity * product.price;
+        cart.updateSubtotal($cartProduct, product);
+        cart.updateTotalAndCartCount();
+      }else {
+        cart.displayPreviousQtyValue($cartProduct, product);
+      }
+    })
+  };
+
+  this.bindEventOnRemoveButton = function ($cartProduct, product) {
+    var cart = this;
+    $cartProduct.find(".remove").on("click", function() {
+      if (confirm("Are you sure want to remove " + product.name + " product from cart!!")) {
+        product.quantity = 0;
+        product.subtotal = 0;
+        cart.removeProductFromMyCartList(product);
+        $cartProduct.remove();
+        cart.updateTotalAndCartCount();
+      }
+    })
+  };
+
   this.displayProducts = function () {
-    var length = productJSON.length;
-    while (length--) {
-      var product = new Product(length);
-      this.currentProduct = product;
+    var productUniqueId = Object.keys(this.productJSON);
+    var lengthOfproductJSON = productUniqueId.length;
+    while (lengthOfproductJSON--) {
+      var product = new Product(this.productJSON[productUniqueId[lengthOfproductJSON]], productUniqueId[lengthOfproductJSON]);
+      this.products[productUniqueId[lengthOfproductJSON]] = product;
       this.displayProduct(product);
     }
   };
+
+  this.displayProduct = function(product) {
+    var $productElem = $( '#productTemplate' ).clone().removeClass( 'hidden' ).addClass("." + product.category);
+    $productElem.find( ".productImg" ).attr({src: product.imgSrc});
+    this.updateProductAttributes(product, $productElem, false);
+    $( '#product' ).removeClass('hidden').append($productElem.attr({id: product.number}));
+  };
 };
+
+Cart.prototype = {
+  updateProductAttributes: function(product, $productElem, isCartProduct) {
+    if (isCartProduct) {
+      this.updateSubtotal($productElem, product);
+      $productElem.find(".qtyInput").val(product.quantity);
+    }
+    $productElem.find('.name').text(product.name);
+    $productElem.find('.category').text(product.category);
+    $productElem.find('.description').text(product.description);
+    $productElem.find('.price').text(product.price);
+  },
+
+  showDefaultList: function () {
+    this.productTab.trigger("click");
+  },
+
+  removeProductFromMyCartList: function (product) {
+    delete this.myCartList[product.number];
+  },
+
+  getProduct: function (productId) {
+    return this.products[productId];
+  },
+
+  isPresentInCart: function (productId) {
+    if (this.myCartList[productId]) return true;
+    else return false;
+  },
+
+  updateTotalAndCartCount: function () {
+    var cart = this, totalQtyInCart = 0;
+    this.total = 0;
+    $.each(this.myCartList, function (key, value) {
+      cart.total += cart.products[key].subTotal;
+      totalQtyInCart += cart.myCartList[key];
+    })
+    $('#total').text("Total : " + this.total);
+    this.myCartTab.text("My Cart " + "(" + totalQtyInCart + ")");
+  },
+
+  showAllProducts: function() {
+    this.productRecords.removeClass("hidden");
+  },
+
+  updateSubtotal: function ($cartProduct, product) {
+    $cartProduct.find(".subTotal").text(product.subTotal);
+  },
+
+  showProductList: function() {
+    $( ".tab" ).removeClass( "selected" );
+    this.productTab.addClass( "selected" );
+    this.myCartProductsDiv.addClass( "hidden" );
+    this.productListSectionDiv.removeClass( "hidden" );
+  },
+
+  displayProductInMyCart: function () {
+    $( ".tab" ).removeClass( "selected" );
+    this.myCartTab.addClass( "selected" );
+    this.productListSectionDiv.addClass( "hidden" );
+    this.myCartProductsDiv.removeClass( "hidden" );
+  },
+
+  searchProductsByCategory: function ($button) {
+    this.productRecords.removeClass("hidden");
+    var inputCategory = $('#search_box').val().trim().toLowerCase();
+    $.each(this.products, function(key, product) {
+      var regex = new RegExp(inputCategory);
+      if (!(regex.test(product.category.toLowerCase()))) {
+        $("#" + product.number).addClass("hidden");
+      }
+    })
+  },
+
+  displayPreviousQtyValue: function ($cartProduct, product) {
+    $cartProduct.find(".qtyInput").val(product.quantity);
+  }
+}
