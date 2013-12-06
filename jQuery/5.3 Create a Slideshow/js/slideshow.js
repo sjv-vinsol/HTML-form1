bd9cd1909ac3f84bdd5b414003f18c8f94ce636c
sjv-vinsol <sanjeev@vinsol.com>
2013-12-06 16:59:57 +0530

    Added document.ready and added whitespaces for readability

--- jQuery/5.3 Create a Slideshow/js/slideshow.js
+++ jQuery/5.3 Create a Slideshow/js/slideshow.js
@@ -1,22 +1,26 @@
-var $slideshow = $("#slideshow").children("li").addClass("hidden"), navArea = $('<div/>', {id: "navigation"});
-var displaySlideshowDiv = $('<div/>').append($slideshow), navigationDelay = 200, displayTime = 1000;
-$("body").prepend(displaySlideshowDiv);
+$(document).ready(function () {
+  var $slideshow = $("#slideshow").children("li").addClass("hidden"), navArea = $('<div/>', {id: "navigation"});
+  var displaySlideshowDiv = $('<div/>').append($slideshow), navigationDelay = 200, displayTime = 1000;
+  $("body").prepend(displaySlideshowDiv);
 
-// add image numbers to navigation area
-for (i = 0; i < $slideshow.length; i++) {
-  var elem = navArea.append($('<span/>', {id: i + 1, text: i + 1, class: "navigation"}));
-}
-var $navigationNumbers = navArea.insertAfter(displaySlideshowDiv).children( "span" );
-$slideshow.first().fadeIn(navigationDelay);
-$navigationNumbers.first().addClass( "redBackground" );
-var indexOfLastElem = $slideshow.last().index();
-setInterval(function(){
-  var $visibleElem = $slideshow.filter( ":visible" );
-  var indexOfVisibleElem = $visibleElem.index();
-  $visibleElem.fadeOut(navigationDelay, function(){
-    $($navigationNumbers[indexOfVisibleElem]).removeClass( "redBackground" ); 
-    var indexOfNextElem = (indexOfVisibleElem == indexOfLastElem) ? 0 : (indexOfVisibleElem + 1);
-    $navigationNumbers.eq(indexOfNextElem).addClass( "redBackground" );
-    $slideshow.eq(indexOfNextElem).fadeIn(navigationDelay);
-  })
-}, displayTime);
+  // add image numbers to navigation area
+  for (i = 0; i < $slideshow.length; i++) {
+    var elem = navArea.append($('<span/>', {id: i + 1, text: i + 1, class: "navigation"}));
+  }
+
+  var $navigationNumbers = navArea.insertAfter(displaySlideshowDiv).children( "span" );
+  $slideshow.first().fadeIn(navigationDelay);
+  $navigationNumbers.first().addClass( "redBackground" );
+  var indexOfLastElem = $slideshow.last().index();
+
+  setInterval(function(){
+    var $visibleElem = $slideshow.filter( ":visible" );
+    var indexOfVisibleElem = $visibleElem.index();
+    $visibleElem.fadeOut(navigationDelay, function(){
+      $($navigationNumbers[indexOfVisibleElem]).removeClass( "redBackground" );
+      var indexOfNextElem = (indexOfVisibleElem == indexOfLastElem) ? 0 : (indexOfVisibleElem + 1);
+      $navigationNumbers.eq(indexOfNextElem).addClass( "redBackground" );
+      $slideshow.eq(indexOfNextElem).fadeIn(navigationDelay);
+    })
+  }, displayTime);
+})
