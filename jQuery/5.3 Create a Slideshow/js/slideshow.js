b47a0ffa4c101a90e1bed2d09f061a1471e1adf8
sjv-vinsol <sanjeev@vinsol.com>
2013-12-06 17:27:52 +0530

    Encapsulate code in many different functions

--- jQuery/5.3 Create a Slideshow/js/slideshow.js
+++ jQuery/5.3 Create a Slideshow/js/slideshow.js
@@ -1,26 +1,39 @@
+var navigationDelay = 200, displayTime = 1000;
 $(document).ready(function () {
-  var $slideshow = $("#slideshow").children("li").addClass("hidden"), navArea = $('<div/>', {id: "navigation"});
-  var displaySlideshowDiv = $('<div/>').append($slideshow), navigationDelay = 200, displayTime = 1000;
+  var $slideshow = $("#slideshow").children("li").addClass("hidden"), navigation = $('<div/>', {id: "navigation"});
+  var displaySlideshowDiv = $('<div/>').append($slideshow);
   $("body").prepend(displaySlideshowDiv);
 
-  // add image numbers to navigation area
-  for (i = 0; i < $slideshow.length; i++) {
-    var elem = navArea.append($('<span/>', {id: i + 1, text: i + 1, class: "navigation"}));
-  }
+  displayImageNumber($slideshow, navigation);
 
-  var $navigationNumbers = navArea.insertAfter(displaySlideshowDiv).children( "span" );
-  $slideshow.first().fadeIn(navigationDelay);
-  $navigationNumbers.first().addClass( "redBackground" );
+  highlightImageNumber(navigation, $slideshow, displaySlideshowDiv);
+  
   var indexOfLastElem = $slideshow.last().index();
+
+  function displayNextElem (indexOfVisibleElem) {
+    $($navigationNumbers[indexOfVisibleElem]).removeClass( "redBackground" );
+    var indexOfNextElem = (indexOfVisibleElem == indexOfLastElem) ? 0 : (indexOfVisibleElem + 1);
+    $navigationNumbers.eq(indexOfNextElem).addClass( "redBackground" );
+    $slideshow.eq(indexOfNextElem).fadeIn(navigationDelay);
+  }
 
   setInterval(function(){
     var $visibleElem = $slideshow.filter( ":visible" );
     var indexOfVisibleElem = $visibleElem.index();
     $visibleElem.fadeOut(navigationDelay, function(){
-      $($navigationNumbers[indexOfVisibleElem]).removeClass( "redBackground" );
-      var indexOfNextElem = (indexOfVisibleElem == indexOfLastElem) ? 0 : (indexOfVisibleElem + 1);
-      $navigationNumbers.eq(indexOfNextElem).addClass( "redBackground" );
-      $slideshow.eq(indexOfNextElem).fadeIn(navigationDelay);
+      displayNextElem(indexOfVisibleElem);
     })
   }, displayTime);
 })
+
+function displayImageNumber ($slideshow, navigation) {
+  for (i = 0; i < $slideshow.length; i++) {
+    var elem = navigation.append($('<span/>', {id: i + 1, text: i + 1, class: "navigation"}));
+  }
+}
+
+function highlightImageNumber (navigation, $slideshow, displaySlideshowDiv) {
+  $navigationNumbers = navigation.insertAfter(displaySlideshowDiv).children( "span" );
+  $slideshow.first().fadeIn(navigationDelay);
+  $navigationNumbers.first().addClass( "redBackground" );
+}
