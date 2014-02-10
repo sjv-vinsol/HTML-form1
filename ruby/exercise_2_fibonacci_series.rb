69fcc6b758566e07138295b6b500e5b2206f58a2
sjv-vinsol <sanjeev@vinsol.com>
2014-02-10 20:05:42 +0530

    completed exercise2 Fibonacci - Yield

---
+++ ruby/exercise_2_fibonacci_series.rb
@@ -0,0 +1,23 @@
+def fibinacci()
+  fib = []
+  continue = true
+  while continue
+    if fib.empty?
+      fib << 0
+      fib << 1
+    else
+      continue = yield(fib)
+    end
+  end
+  p fib.join(",")
+end
+
+fibinacci() do |fib|
+  index_of_last_element = fib.length - 1
+  sum_of_last_two_elements = fib[index_of_last_element] + fib[index_of_last_element-1]
+  if sum_of_last_two_elements < 1000
+    fib << sum_of_last_two_elements
+  else
+    false
+  end
+end
