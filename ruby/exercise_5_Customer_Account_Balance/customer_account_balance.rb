fcd8ce6b207219f25c87084033628fcfc8da288c
sjv-vinsol <sanjeev@vinsol.com>
2014-02-12 15:48:21 +0530

    Completed exercise customer_account_balance

---
+++ ruby/exercise_5_Customer_Account_Balance/customer_account_balance.rb
@@ -0,0 +1,24 @@
+class Customer
+  attr_accessor :name, :balance
+  attr_reader :account_number
+
+  @@account_number_counter = 0
+
+  def initialize(name)
+    @name = name
+    @account_number = (@@account_number_counter += 1)
+    @balance = 1000
+  end
+
+  def deposit(amount)
+    @balance += amount
+  end
+
+  def withdraw(amount)
+    if amount > @balance
+      p "You can only withdraw amount upto #{@amount}"
+    else
+      @balance -= amount
+    end
+  end
+end
