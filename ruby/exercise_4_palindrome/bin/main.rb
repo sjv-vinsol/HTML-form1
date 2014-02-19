ae7fdd8d4ab9b25ebdde342bb957e96b2956b414
sjv-vinsol <sanjeev@vinsol.com>
2014-02-19 22:00:43 +0530

    Code optimize
    - Used single quotes instead of double quotes

--- ruby/exercise_4_palindrome/bin/main.rb
+++ ruby/exercise_4_palindrome/bin/main.rb
@@ -1,13 +1,13 @@
 require_relative '../lib/string'
 
-p "Enter a string"
+p 'Enter a string'
 begin 
   input_string = gets.chomp.downcase
 end while input_string.empty?
 
 if input_string == 'q'
-  p "Good Bye!!!"
+  p 'Good Bye!!!'
   exit()
 else
-  p (input_string.palindrome?) ? "String is palindrome" : "String is not a palindrome"
+  p (input_string.palindrome?) ? 'String is palindrome' : 'String is not a palindrome'
 end
