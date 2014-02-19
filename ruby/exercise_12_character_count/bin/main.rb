ac22ecefcd48dae4f87fb20fccb7da319e9b2bfe
sjv-vinsol <sanjeev@vinsol.com>
2014-02-19 16:20:15 +0530

    Created method count_by_type in String class
    This method is used to calculate the count of lowercase, uppercase, integers,
    and special characters in a string.

---
+++ ruby/exercise_12_character_count/bin/main.rb
@@ -0,0 +1,8 @@
+require_relative "../lib/string"
+
+begin
+  p "Enter a string"
+  input_string = gets.chomp
+end while input_string.empty?
+
+p input_string.count_by_type
---
+++ ruby/exercise_12_character_count/lib/string.rb
@@ -0,0 +1,29 @@
+class String
+  def digit?
+    (0..9).any? {|int| int.to_s == self}
+  end
+
+  def uppercase?
+    ("A".."Z").any? { |char| char == self }
+  end
+
+  def downcase?
+    ("a".."z").any? { |char| char == self }
+  end
+
+  def count_by_type
+    count = {:upper => 0, :lower => 0, :integer => 0, :special_chars => 0}
+    self.each_char do |c|
+      if c.digit?
+        count[:integer] += 1
+      elsif c.uppercase?
+        count[:upper] += 1
+      elsif c.downcase?
+        count[:lower] += 1
+      else
+        count[:special_chars] += 1
+      end
+    end
+    count
+  end
+end
