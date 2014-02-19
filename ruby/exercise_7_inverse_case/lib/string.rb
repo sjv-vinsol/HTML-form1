149d053408b17d84518db105d7a90db063e91e94
sjv-vinsol <sanjeev@vinsol.com>
2014-02-19 19:58:24 +0530

    Code optimization
    - Overwritten default to_s method and used "swapcase" method.

--- ruby/exercise_7_inverse_case/lib/string.rb
+++ ruby/exercise_7_inverse_case/lib/string.rb
@@ -1,13 +1,5 @@
 class String 
-  def upper?
-    self == self.upcase
-  end
-
-  def inverse_case
-    arr = []
-    self.each_char do |c|
-      c.upper? ? arr << c.downcase : arr << c.upcase
-    end
-    arr.join()
+  def to_s
+    self.swapcase
   end
 end
