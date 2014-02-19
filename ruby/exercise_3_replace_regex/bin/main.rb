b38cad8371aef1309eda1cecaf756fab92df499e
sjv-vinsol <sanjeev@vinsol.com>
2014-02-19 21:55:24 +0530

    Oprimized code
    - used single quotes instead of double quotes
    - removed self from method 'replace_pattern'

--- ruby/exercise_3_replace_regex/bin/main.rb
+++ ruby/exercise_3_replace_regex/bin/main.rb
@@ -1,5 +1,5 @@
-require_relative "../lib/string"
+require_relative '../lib/string'
 
-p "Enter a string"
+p 'Enter a string'
 regex = /[aeiou]/
-p gets.chomp.replace_pattern(regex, "*")
+p gets.chomp.replace_pattern(regex, '*')
--- ruby/exercise_3_replace_regex/lib/string.rb
+++ ruby/exercise_3_replace_regex/lib/string.rb
@@ -1,5 +1,5 @@
 class String
   def replace_pattern(regex, replace_by)
-    self.gsub(regex, replace_by)
+    gsub(regex, replace_by)
   end
 end
