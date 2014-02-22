d2507aa000a102e366750551cbc7f8b2f0cf8e4d
sjv-vinsol <sanjeev@vinsol.com>
2014-02-22 10:46:58 +0530

    Code optimize
    - Used ruby prime? method

--- ruby/exercise_15_prime_numbers/lib/integer.rb
+++ ruby/exercise_15_prime_numbers/lib/integer.rb
@@ -1,12 +1,10 @@
+require 'prime'
+
 class Integer
   def prime_numbers
     prime_no = [(2 if self >= 2)]
     (3..self).step(2) do |number|
-      prime = true
-      (2..(number/2)).step do |devisor|
-          prime = false if number % devisor == 0
-      end
-      prime_no << number if prime
+      prime_no << number if Prime.prime?(number)
     end
     prime_no
   end
