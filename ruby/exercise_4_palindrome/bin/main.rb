e2587172c19c93d2c4dfae7982e82298c56976a1
sjv-vinsol <sanjeev@vinsol.com>
2014-02-24 16:08:00 +0530

    replace p with puts

--- ruby/exercise_4_palindrome/bin/main.rb
+++ ruby/exercise_4_palindrome/bin/main.rb
@@ -1,13 +1,13 @@
 require_relative '../lib/string'
 
-p 'Enter a string'
+puts 'Enter a string'
 begin 
   input_string = gets.chomp.downcase
 end while input_string.empty?
 
 if input_string == 'q'
-  p 'Good Bye!!!'
+  puts 'Good Bye!!!'
   exit()
 else
-  p (input_string.palindrome?) ? 'String is palindrome' : 'String is not a palindrome'
+  puts (input_string.palindrome?) ? 'String is palindrome' : 'String is not a palindrome'
 end
