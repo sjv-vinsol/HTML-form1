ea2a14a8683716b8aa08989b51d754b9a41c88c5
sjv-vinsol <sanjeev@vinsol.com>
2014-02-24 12:23:45 +0530

    Code optimize
    - Remove unnecessary methods.
    - Use case statement instead of if else.

--- ruby/exercise_12_character_count/lib/string.rb
+++ ruby/exercise_12_character_count/lib/string.rb
@@ -1,24 +1,13 @@
 class String
-  def digit?
-    (0..9).any? { |int| int.to_s == self }
-  end
-
-  def uppercase?
-    ('A'..'Z').any? { |char| char == self }
-  end
-
-  def downcase?
-    ('a'..'z').any? { |char| char == self }
-  end
-
   def count_by_type
-    count = { :upper => 0, :lower => 0, :integer => 0, :special_chars => 0 }
+    count = { upper: 0, lower: 0, integer: 0, special_chars: 0 }
     each_char do |c|
-      if c.digit?
+      case c
+      when (/\d/)
         count[:integer] += 1
-      elsif c.uppercase?
+      when ('A'..'Z')
         count[:upper] += 1
-      elsif c.downcase?
+      when ('a'..'z')
         count[:lower] += 1
       else
         count[:special_chars] += 1
