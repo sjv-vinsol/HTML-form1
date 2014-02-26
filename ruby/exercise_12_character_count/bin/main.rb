bf372bdfe06e2d45cae89f6f81c7a1298308ea4d
sjv-vinsol <sanjeev@vinsol.com>
2014-02-26 12:07:48 +0530

    Code optimize
    - Initialize hash with '0'
    - Use range ('0'..'9') instead of regex for the same
    - Increase readability by using 'when' 'then' statement
    - change method name 'count_by_type' -> 'count_chars_by_type'

--- ruby/exercise_12_character_count/bin/main.rb
+++ ruby/exercise_12_character_count/bin/main.rb
@@ -1,8 +1,6 @@
 require_relative '../lib/string'
 
-begin
-  p 'Enter a string'
-  input_string = gets.chomp
-end while input_string.empty?
+p 'Enter a string'
+input_string = gets.chomp
 
-p input_string.count_by_type
+p input_string.count_chars_by_type
--- ruby/exercise_12_character_count/lib/string.rb
+++ ruby/exercise_12_character_count/lib/string.rb
@@ -1,16 +1,12 @@
 class String
-  def count_by_type
-    count = { upper: 0, lower: 0, integer: 0, special_chars: 0 }
+  def count_chars_by_type
+    count = Hash.new(0)
     each_char do |c|
       case c
-      when (/\d/)
-        count[:integer] += 1
-      when ('A'..'Z')
-        count[:upper] += 1
-      when ('a'..'z')
-        count[:lower] += 1
-      else
-        count[:special_chars] += 1
+      when ('0'..'9') then count[:integer] += 1
+      when ('A'..'Z') then count[:upper] += 1
+      when ('a'..'z') then count[:lower] += 1
+      else  count[:special_chars] += 1
       end
     end
     count
