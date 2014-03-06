35064318ed8b745a48349fb6860715295c1777c9
sjv-vinsol <sanjeev@vinsol.com>
2014-03-06 12:47:32 +0530

    - initialize interest using block - create method '#simple_interest' and '#compound_interest' - rate is initialize as constant. - create method '#to_s' to return diff in interest.

--- ruby/exercise_20_interest_difference/lib/interest.rb
+++ ruby/exercise_20_interest_difference/lib/interest.rb
@@ -1,9 +1,24 @@
 class Interest
   attr_accessor :principal, :time, :rate
+  RATE = 10
 
-  def diff_in_compound_and_simple()
-    simple_interest = @principal * @time * @rate
-    compound_interest = @principal * (((1 + @rate) ** @time) - 1)
+  def initialize
+    @principal, @time, @rate = yield << RATE / 100.to_f
+  end
+
+  def diff_in_compound_and_simple
     (compound_interest - simple_interest).round(2)
+  end
+
+  def simple_interest
+    (principal * time * rate).round(2)
+  end
+
+  def compound_interest
+    (principal * (((1 + rate) ** time) - 1)).round(2)
+  end
+
+  def to_s
+    "Difference in compound and simple interest: #{diff_in_compound_and_simple}"
   end
 end
